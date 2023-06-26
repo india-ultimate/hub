ff9a0ed01c8a8b734516f398c56e1da109d981ff
Tejaa Chintaluri <saitejaac@gmail.com>
2023-06-26 22:09:21 +0530

    change: retained only required fields in classes; black formatting

--- server/forms.py
+++ server/forms.py
@@ -1,58 +1,65 @@
 from django import forms
 from .models import User, Player, Membership, Vaccination
 
-GENDER_CHOICES = [('male', 'Male'), ('female', 'Female'), ('other', 'Other')]
-
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['email', 'phone']
+        fields = ["email", "phone"]
 
 
 class PlayerForm(forms.ModelForm):
-    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
-    gender = forms.ChoiceField(choices=GENDER_CHOICES)
-    other_gender = forms.CharField(required=False, max_length=20)
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
 
     class Meta:
         model = Player
-        fields = ['first_name', 'last_name', 'date_of_birth', 'gender', 'other_gender','city', 'state_ut', 'team_name',
-                  'occupation', 'educational_institution', 'india_ultimate_profile']
+        fields = [
+            "first_name",
+            "last_name",
+            "date_of_birth",
+            "gender",
+            "city",
+            "state_ut",
+            "team_name",
+            "occupation",
+            "educational_institution",
+            "india_ultimate_profile",
+        ]
         labels = {
-            'state_ut' : 'State/UT',
+            "state_ut": "State/UT",
         }
-
-    def clean(self):
-        cleaned_data = super().clean()
-        gender = cleaned_data.get('gender')
-        other_gender = cleaned_data.get('other_gender')
-
-        if gender == 'other' and not other_gender:
-            self.add_error('other_gender', 'Please specify the gender.')
-
-        return cleaned_data
 
 
 class MembershipForm(forms.ModelForm):
-    start_date = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
-    end_date = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
+    start_date = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date"}), initial="2023-04-01"
+    )
+    end_date = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date"}), initial="2024-03-31"
+    )
+
     class Meta:
         model = Membership
-        fields = ['membership_number', 'is_annual', 'start_date', 'end_date', 'is_active']
+        fields = [
+            "start_date",
+            "end_date",
+        ]
         labels = {
-            'is_annual' : 'Is Annual membership',
-            'start_date' : 'Membership start date',
-            'end_date' : 'Membership end date',
-            'is_active' : 'Membership currently active'
+            "start_date": "Membership start date",
+            "end_date": "Membership end date",
         }
 
 
 class VaccinationForm(forms.ModelForm):
     class Meta:
         model = Vaccination
-        fields = ['is_vaccinated', 'vaccination_name', 'vaccination_certificate', 'explain_not_vaccinated']
-        label = {
-            'vaccination_certificate': 'Please upload your vaccination certificate',
-            'explain_not_vaccinated': 'Explanation for not being vaccinated',
+        fields = [
+            "is_vaccinated",
+            "vaccination_name",
+            "vaccination_certificate",
+            "explain_not_vaccinated",
+        ]
+        labels = {
+            "vaccination_certificate": "Please upload your vaccination certificate",
+            "explain_not_vaccinated": "Explanation for not being vaccinated",
         }
