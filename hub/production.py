1bb9ca972034905e66d8d87acbef837e9f574fdf
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-06-28 10:57:07 +0530

    Set MEDIA_ROOT in production settings

--- hub/production.py
+++ hub/production.py
@@ -9,4 +9,7 @@ CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
-DATABASES["default"]["NAME"] = Path("/data") / "production.db.sqlite"
+
+DATA_DIR = Path("/data")
+DATABASES["default"]["NAME"] = DATA_DIR / "production.db.sqlite"
+MEDIA_ROOT = DATA_DIR / "media"
