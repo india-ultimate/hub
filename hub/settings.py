434eac3dbcb7b686f1ba0dc924eac7b414764698
Tejaa Chintaluri <saitejaac@gmail.com>
2023-06-28 22:44:53 +0530

    Enable vaccination certificate uploads

--- hub/settings.py
+++ hub/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# File upload settings
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/uploads/"
