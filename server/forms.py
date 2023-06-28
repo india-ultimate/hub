962030cee124dcf369e9466df885464eb1a567b4
Tejaa Chintaluri <saitejaac@gmail.com>
2023-06-28 12:20:41 +0530

    In forms.py add proper error message to fields for incomplete forms

--- server/forms.py
+++ server/forms.py
@@ -36,10 +36,12 @@ class PlayerForm(forms.ModelForm):
         state_ut = cleaned_data.get("state_ut")
         not_in_india = cleaned_data.get("not_in_india")
 
-        if (not state_ut and not not_in_india) or (state_ut and not_in_india):
-            raise forms.ValidationError(
-                "State/UT & 'Not in India' cannot be both checked or cannot be both empty"
-            )
+        if not state_ut and not not_in_india:
+            self.add_error("state_ut", "Please select a State/UT")
+            self.add_error("not_in_india", "Are you not in India? Please select so.")
+        elif state_ut and not_in_india:
+            self.add_error("state_ut", "State/UT already selected")
+            self.add_error("not_in_india", "Are you in India?")
 
         return cleaned_data
 
@@ -77,3 +79,22 @@ class VaccinationForm(forms.ModelForm):
             "vaccination_certificate": "Please upload your vaccination certificate",
             "explain_not_vaccinated": "Explanation for not being vaccinated",
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        is_vaccinated = cleaned_data.get("is_vaccinated")
+        explain_not_vaccinated = cleaned_data.get("explain_not_vaccinated")
+        vaccination_name = cleaned_data.get("vaccination_name")
+
+        if not is_vaccinated and not explain_not_vaccinated:
+            self.add_error("is_vaccinated", "Please check box")
+            self.add_error("vaccination_name", "Select you vaccination name")
+        elif is_vaccinated and explain_not_vaccinated:
+            self.add_error("vaccination_name", "Select your vaccination name")
+            self.add_error(
+                "explain_not_vaccinated", "No explanation needed if you are vaccinated!"
+            )
+        elif is_vaccinated and not vaccination_name:
+            self.add_error("vaccination_name", "Select vaccination name")
+
+        return cleaned_data
