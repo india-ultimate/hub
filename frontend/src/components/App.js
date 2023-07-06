3bd3e7065c6760400baa273221993ac7a1a93e7f
Tejaa Chintaluri <saitejaac@gmail.com>
2023-07-06 18:41:41 +0530

    Add a Header component with navbar

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,20 +1,23 @@
 import { lazy } from "solid-js";
 import { Routes, Route } from "@solidjs/router";
+import Header from "./Header";
 const Login = lazy(() => import("./Login"));
 const Home = lazy(() => import("./Home"));
 
 export default function App() {
   return (
     <div>
-      <h1>Welcome to India Ultimate Hub!</h1>
-      <Routes>
-        <Route path="/login" component={Login} />
-        <Route path="/" component={Home} />
-        <Route
-          path="/about"
-          element={<div>This site was made with Solid</div>}
-        />
-      </Routes>
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Home} />
+          <Route
+            path="/about"
+            element={<div>This site was made with Solid</div>}
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
