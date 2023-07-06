65fe4c9292dcc21fbe01279f074eff573cc664ed
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-07-07 01:52:14 +0530

    Add API end-point for firebase login

--- server/api.py
+++ server/api.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, get_user_model, login, logout
+from firebase_admin import auth
 from ninja import ModelSchema, NinjaAPI, Schema
 from ninja.security import django_auth
 
@@ -10,6 +11,11 @@ api = NinjaAPI(auth=django_auth, csrf=True)
 class Credentials(Schema):
     username: str
     password: str
+
+
+class FirebaseCredentials(Schema):
+    token: str
+    uid: str
 
 
 class Response(Schema):
@@ -40,3 +46,22 @@ def api_login(request, credentials: Credentials):
 @api.post("/logout")
 def api_login(request):
     logout(request)
+
+
+@api.post("/firebase-login", auth=None, response={200: UserSchema, 403: Response})
+def login(request, credentials: FirebaseCredentials):
+    firebase_user = auth.get_user(credentials.uid)
+    invalid_credentials = 403, {"message": "Invalid credentials"}
+    if firebase_user is None:
+        return invalid_credentials
+
+    try:
+        # FIXME: Handle login using email
+        # FIXME: Decide on how to handle new sign-ups
+        user = User.objects.get(phone=firebase_user.phone_number)
+    except User.DoesNotExist:
+        return invalid_credentials
+
+    request.session["firebase_token"] = credentials.token
+    request.user = user
+    return 200, UserSchema.from_orm(user)
