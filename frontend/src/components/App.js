5093858c5cfe3a06374b1c5227f9b34c6809b2c7
Tejaa Chintaluri <saitejaac@gmail.com>
2023-07-06 18:41:41 +0530

    Add header and footer in App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,17 @@
 import { lazy } from "solid-js";
 import { Routes, Route } from "@solidjs/router";
+import Header from "./Header";
+import Footer from "./Footer";
+
 const Login = lazy(() => import("./Login"));
 const Home = lazy(() => import("./Home"));
 
 export default function App() {
   return (
     <div>
-      <h1>Welcome to India Ultimate Hub!</h1>
+      <Header />
+      {/* Routing */}
+      <div>
       <Routes>
         <Route path="/login" component={Login} />
         <Route path="/" component={Home} />
@@ -15,6 +20,9 @@ export default function App() {
           element={<div>This site was made with Solid</div>}
         />
       </Routes>
+      </div>
+
+      <Footer/>
     </div>
   );
 }
