25350986dfb2ecac1d89b33a19e1c55cb34fa1df
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-07-10 17:11:34 +0530

    Force user to login to view the Home page
    This is a trivial change to the home page view to verify that the login
    workflow is working correctly.
    
    The logged in status and the data returned by the `/api/user` end-point are not
    stored in a global store, and therefore the API calls are made multiple times
    to the end-point, but this can be fixed in a follow-up commit.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,43 @@
+import { useNavigate } from "@solidjs/router";
+import { createSignal, createEffect } from "solid-js";
+
 const Home = () => {
+  // FIXME: store this information in a solidjs store, instead!
+  const [loggedIn, setLoggedIn] = createSignal(true);
+  const [data, setData] = createSignal({});
+
+  createEffect(() => {
+    if (!loggedIn()) {
+      const navigate = useNavigate();
+      navigate("/login", { replace: true });
+    }
+  });
+
+  const response = fetch("/api/user", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+    credentials: "same-origin"
+  })
+    .then(response => {
+      if (response.status == 200) {
+        setLoggedIn(true);
+        response.json().then(data => {
+          console.log(data);
+          setData(data);
+        });
+      } else {
+        setLoggedIn(false);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      setLoggedIn(false);
+    });
   return (
     <div>
-      <h1 class="text-4xl font-bold mb-4 text-red-500">Welcome!</h1>
+      <h1 class="text-4xl font-bold mb-4 text-red-500">
+        Welcome, {data().username}!
+      </h1>
     </div>
   );
 };
--- server/api.py
+++ server/api.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import authenticate, login
-from ninja import NinjaAPI, Schema
+from django.contrib.auth import authenticate, get_user_model, login
+from ninja import ModelSchema, NinjaAPI, Schema
 from ninja.security import django_auth
+
+
+User = get_user_model()
 
 api = NinjaAPI(auth=django_auth, csrf=True)
 
@@ -14,9 +17,15 @@ class Response(Schema):
     message: str
 
 
+class UserSchema(ModelSchema):
+    class Config:
+        model = User
+        model_fields = ["username"]
+
+
 @api.get("/user")
-def current_user(request):
-    return f"Authenticated user {request.auth}"
+def current_user(request, response={200: UserSchema}):
+    return UserSchema.from_orm(request.user)
 
 
 @api.post("/login", auth=None, response={200: Response, 403: Response})
