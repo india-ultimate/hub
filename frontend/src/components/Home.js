e068e0db20f2974e93dfa6ff3a5dd92fa8a4e1b2
Tejaa Chintaluri <saitejaac@gmail.com>
2023-07-13 11:11:51 +0530

    Link to registration on home page if unregistered

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "@solidjs/router";
+import { useNavigate, A } from "@solidjs/router";
 import { createSignal, createEffect, onMount, Show } from "solid-js";
 import { getCookie, fetchUserData } from "../utils";
 import { useStore } from "../store";
@@ -39,8 +39,26 @@ const Home = () => {
       <h1 class="text-4xl font-bold mb-4 text-red-500">
         Welcome, {store.data.first_name} {store.data.last_name}!
       </h1>
-      <Show when={store.data.player}>
-        <Player player={store.data.player} />
+      <Show when={store.data.is_player}>
+        <div>
+          <Show when={!store.data.player}
+                fallback={
+                  <div>
+                    <h3 class="text-2xl font-bold mb-4 text-blue-500">
+                      You are registered to play.
+                    </h3>
+                    <Player player={store.data.player} />
+                  </div>
+                }
+          >
+             <h3 class="text-2xl font-bold mb-4">
+              Want to play?{" "}
+              <A href="/registration" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                Register!
+              </A>
+            </h3>
+          </Show>
+        </div>
       </Show>
       <button class="my-10" onClick={logout}>
         Logout
