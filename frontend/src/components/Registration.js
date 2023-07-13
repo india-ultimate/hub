d3fc10f4af219683e94f2cefbe8e0ce5f9f4d1f5
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-07-13 10:17:38 +0530

    registration: Use the Show component instead of && condition

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -257,7 +257,7 @@ const RegistrationForm = () => {
               </label>
             </div>
             <div>
-              {gender() === "O" && (
+              <Show when={gender() === "O"}>
                 <label
                   for="other-gender"
                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -272,7 +272,7 @@ const RegistrationForm = () => {
                     required
                   />
                 </label>
-              )}
+              </Show>
             </div>
             <div>
               <div class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
@@ -310,37 +310,35 @@ const RegistrationForm = () => {
                 placeholder=""
               />
             </div>
-            <div>
-              {/* State dropdown */}
-              {!notInIndia() && (
-                <div>
-                  <label
-                    for="state"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    State
-                  </label>
-                  <select
-                    id="state"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    value={state()}
-                    onInput={e => setState(e.target.value)}
-                    required
-                  >
-                    <option value="">Select State/UT</option>
-                    {stateChoices.map(choice => (
-                      <option value={choice.value}>{choice.label}</option>
-                    ))}
-                  </select>
-                </div>
-              )}
-            </div>
+            {/* State dropdown */}
+            <Show when={!notInIndia()}>
+              <div>
+                <label
+                  for="state"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  State
+                </label>
+                <select
+                  id="state"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={state()}
+                  onInput={e => setState(e.target.value)}
+                  required
+                >
+                  <option value="">Select State/UT</option>
+                  {stateChoices.map(choice => (
+                    <option value={choice.value}>{choice.label}</option>
+                  ))}
+                </select>
+              </div>
+            </Show>
           </div>
-          {error() && (
+          <Show when={error()}>
             <p class="my-2 text-sm text-red-600 dark:text-red-500">
               <span class="font-medium">Oops!</span> {error()}
             </p>
-          )}
+          </Show>
           <button
             type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
