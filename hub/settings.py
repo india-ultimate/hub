15c897cc1134fb48f069b56d401dc0b266001882
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-07-13 18:57:11 +0530

    app: Set APP_NAME to UPAI Hub

--- hub/settings.py
+++ hub/settings.py
@@ -151,3 +151,5 @@ STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {"BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"},
 }
+
+APP_NAME = "UPAI Hub"
