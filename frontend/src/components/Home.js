131f80add85a5dc3dfb68960294605afd83d2110
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-07-24 18:06:12 +0530

    home: Display a table of register actions that a user can perform

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -25,26 +25,65 @@ const Home = () => {
       <h1 class="text-4xl font-bold mb-4 text-red-500">
         Welcome {store?.data?.full_name || store?.data?.username}!
       </h1>
-      <div>
-        <Show
-          when={!store.data.player}
-          fallback={
-            <div>
-              <Player player={store.data.player} />
-            </div>
-          }
-        >
-          <h3 class="text-2xl font-bold mb-4">
-            Want to play?{" "}
-            <A
-              href="/registration"
-              class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-            >
-              Register!
-            </A>
-          </h3>
-        </Show>
+      <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <tbody>
+            <Show when={!store.data.player}>
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th
+                  scope="row"
+                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  <A
+                    href="/registration"
+                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >
+                    Register yourself as a player
+                  </A>
+                </th>
+              </tr>
+            </Show>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                <A
+                  href=""
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-not-allowed"
+                >
+                  Fill the membership form for another player
+                </A>
+              </th>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                <A
+                  href=""
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-not-allowed"
+                  disabled
+                >
+                  Fill the membership form as a guardian
+                </A>
+              </th>
+              <td class="px-6 py-4" />
+            </tr>
+          </tbody>
+        </table>
       </div>
+      <Show when={store.data.player}>
+        <div class="mt-5">
+          <h2 class="text-2xl font-bold mb-4 text-black-500">
+            Player information
+          </h2>
+          <div>
+            <Player player={store.data.player} />
+          </div>
+        </div>
+      </Show>
     </div>
   );
 };
