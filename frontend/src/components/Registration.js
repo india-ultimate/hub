0a44c4d1d3b656edf28d27c22b7f4b3abbf00434
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-07-25 09:55:19 +0530

    registration: Change id for confirm email input

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -149,14 +149,14 @@ const RegistrationForm = ({ others }) => {
               </div>
               <div>
                 <label
-                  for="email"
+                  for="confirmEmail"
                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Confirm Email
                 </label>
                 <input
                   type="email"
-                  id="email"
+                  id="confirmEmail"
                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="foo@example.com"
                   value={emailConfirm()}
