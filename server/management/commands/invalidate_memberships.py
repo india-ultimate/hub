eceb5c1d618389b93ec53e9d4ba82aa3d0da401a
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-02 18:37:22 +0530

    management: Add command to invalidate expired memberships

---
+++ server/management/commands/invalidate_memberships.py
@@ -0,0 +1,18 @@
+import datetime
+
+from django.core.management.base import BaseCommand
+from server.models import Membership
+
+
+class Command(BaseCommand):
+    help = "Invalidate memberships whose end date has passed"
+
+    def handle(self, *args, **options):
+        today = datetime.date.today()
+        active_memberships = Membership.objects.filter(end_date__lt=today, is_active=True)
+        n = active_memberships.count()
+        if n > 0:
+            active_memberships.update(is_active=False)
+            self.stdout.write(self.style.SUCCESS(f"Invalidated {n} memberships"))
+        else:
+            self.stdout.write(self.style.NOTICE("No outdated memberships found"))
