5f9e9e37452314395c4f026f699cb467343b98ef
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-07 14:06:14 +0530

    footer: Remove year from the copyright

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
+            ©{" "}
             <A href="/" class="hover:underline">
               UPAI
             </A>
