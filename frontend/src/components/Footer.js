f7cf2a47b6bbf6e20cd965d4f5a7dd60f04f9325
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-07 14:06:33 +0530

    footer: Remove Play Ultimate from the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,7 +10,6 @@ export default function Footer() {
             <A href="/" class="hover:underline">
               UPAI
             </A>
-            . Play Ultimate.
           </span>
           <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
