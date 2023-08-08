c69c91a59a663fd02bf3df4533b83855e7873740
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-08 10:13:14 +0530

    mypy: Add type annotations for the views code

--- server/views.py
+++ server/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def home(request):
+def home(request: HttpRequest) -> HttpResponse:
     return render(request, "index.html")
