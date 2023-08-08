12d57af2fd9ba32f5f1ecd25e5000885c5aa0918
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-08 15:32:17 +0530

    icons: Switch to using heroicons instead of handwritten SVG

--- frontend/src/components/GroupMembership.js
+++ frontend/src/components/GroupMembership.js
@@ -25,6 +25,8 @@ import {
 } from "../constants";
 import MembershipPlayerList from "./MembershipPlayerList";
 import { initFlowbite } from "flowbite";
+import { Icon } from "solid-heroicons";
+import { chevronDown, magnifyingGlass } from "solid-heroicons/solid-mini";
 
 const GroupMembership = () => {
   const [store, { setLoggedIn, setData, setPlayerById }] = useStore();
@@ -157,21 +159,7 @@ const GroupMembership = () => {
           type="button"
         >
           Select Players
-          <svg
-            class="w-2.5 h-2.5 ml-2.5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 6"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 1 4 4 4-4"
-            />
-          </svg>
+          <Icon path={chevronDown} style="width: 24px" />
         </button>
         <div
           id="playerSearch"
@@ -183,21 +171,7 @@ const GroupMembership = () => {
             </label>
             <div class="relative">
               <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg
-                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
+                <Icon path={magnifyingGlass} style="width: 24px" />
               </div>
               <input
                 type="text"
