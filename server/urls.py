5077654f3556de149f31e5cc6e4cd959603c51e3
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-08 14:20:45 +0530

    mypy: Add type annotations to urls.py

--- server/urls.py
+++ server/urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.shortcuts import redirect
-from django.urls import path
+from django.urls import URLPattern, URLResolver, path  # noqa
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 from . import views
 from .api import api
 
-urlpatterns = [path("api/", api.urls)]
+urlpatterns = [path("api/", api.urls)]  # type: list[URLPattern | URLResolver]
 
 if settings.DEBUG:
     urlpatterns.append(path("", ensure_csrf_cookie(lambda req: redirect("http://localhost:3000"))))
