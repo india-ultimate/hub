b928c11588eafdb1452e9a6cded68d047fa5a5ce
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-10 09:56:22 +0530

    text-input: Change the opacity of the placeholder text

--- frontend/src/components/TextInput.js
+++ frontend/src/components/TextInput.js
@@ -38,7 +38,7 @@ const TextInput = props => {
       <input
         {...inputProps}
         class={clsx(
-          "h-14 w-full rounded-2xl border-2 bg-white px-5 outline-none placeholder:text-slate-500 dark:bg-gray-900 md:h-16 md:text-lg lg:h-[70px] lg:px-6 lg:text-xl",
+          "h-14 w-full rounded-2xl border-2 bg-white px-5 outline-none placeholder:text-slate-300 dark:bg-gray-900 md:h-16 md:text-lg lg:h-[70px] lg:px-6 lg:text-xl",
           props.error
             ? "border-red-600/50 dark:border-red-400/50"
             : "border-slate-200 hover:border-slate-300 focus:border-sky-600/50 dark:border-slate-800 dark:hover:border-slate-700 dark:focus:border-sky-400/50"
