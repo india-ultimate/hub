640c090ae7b27f964f4dac1fef3c1b1a63ff40a5
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-24 18:38:34 +0530

    registration: Change minAge to 12 from 13

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -78,4 +78,4 @@ export const annualMembershipFee = 650 * 100; // needs to be in Paise, not Rupee
 export const sponsoredAnnualMembershipFee = 350 * 100; // needs to be in Paise, not Rupees
 export const eventMembershipFee = 375 * 100; // needs to be in Paise, not Rupees
 
-export const minAge = 13; // Minimum age to register a player
+export const minAge = 12; // Minimum age to register a player
