37717569d468829a7b0a2086a173cf7a19fe1e43
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-24 17:59:36 +0530

    player: Use player name as table header instead of field/value

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -31,23 +31,14 @@ const Player = props => {
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" class="px-6 py-3">
-              Field
+              Name
             </th>
             <th scope="col" class="px-6 py-3">
-              Value
+              {props.player?.full_name}
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Name
-            </th>
-            <td class="px-6 py-4">{props.player?.full_name}</td>
-          </tr>
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <th
               scope="row"
