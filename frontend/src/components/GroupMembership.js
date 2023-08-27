b4d8b0923b5c326ba85216be299173435b7f2c8e
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-08-27 19:36:09 +0530

    ui: search input and footer upd

--- frontend/src/components/GroupMembership.js
+++ frontend/src/components/GroupMembership.js
@@ -62,7 +62,7 @@ const PlayerSearchDropdown = props => {
               id="input-group-search"
               class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search player"
-              onChange={e => setSearchText(e.target.value)}
+              onInput={e => setSearchText(e.target.value)}
             />
           </div>
         </div>
