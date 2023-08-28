1814903756080e337924bf38e7c5ac28378ec909
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-28 10:22:40 +0530

    login: Alert document cookies to debug login failures

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -390,6 +390,7 @@ const Login = () => {
           class="mx-2 text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-small rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           onClick={() => {
             setError(false);
+            alert(document.cookie);
             clearCookie("csrftoken");
           }}
         >
