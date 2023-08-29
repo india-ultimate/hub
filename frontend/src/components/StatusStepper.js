1f6b7816f3817bc6c223a6e7f488a189f3166faf
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-29 10:05:56 +0530

    profile-status-stepper: Add a progress bar to show completion status
    Closes #64

--- frontend/src/components/StatusStepper.js
+++ frontend/src/components/StatusStepper.js
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { Icon } from "solid-heroicons";
 import { A } from "@solidjs/router";
+import { Switch, Match } from "solid-js";
 
 import {
   identification,
@@ -38,42 +39,84 @@ const Step = props => {
 };
 
 const StatusStepper = props => {
+  const status = {
+    profile: !!props?.player,
+    vaccine: !!props?.player?.vaccination,
+    ucLink: !!props?.player?.ultimate_central_id,
+    membership: props?.player?.membership?.is_active,
+    waiver: props?.player?.membership?.waiver_valid
+  };
+  const percent = Math.round(
+    (Object.values(status).filter(x => x).length * 100) /
+      Object.keys(status).length
+  );
   return (
-    <ol class="items-center w-full my-4 sm:flex sm:space-x-8">
-      <Step
-        title="Profile Info"
-        link="/"
-        icon={identification}
-        color={props?.player ? "green" : "red"}
-      />
-      <Step
-        title="Vaccine Info"
-        icon={props?.player?.vaccination ? shieldCheck : shieldExclamation}
-        link={`/vaccination/${props.player.id}`}
-        color={props?.player?.vaccination ? "green" : "red"}
-      />
-      <Step
-        title="Ultimate Central profile"
-        icon={arrowTopRightOnSquare}
-        link={`/uc-login/${props.player.id}`}
-        color={props?.player?.ultimate_central_id ? "green" : "red"}
-      />
-      <Step
-        title="Membership info"
-        icon={currencyRupee}
-        link={`/membership/${props.player.id}`}
-        color={props?.player?.membership?.is_active ? "green" : "red"}
-      />
-      <Step
-        title="Liability Waiver"
-        icon={
-          props?.player?.membership?.waiver_valid ? handThumbUp : handThumbDown
-        }
-        color={props?.player?.membership?.waiver_valid ? "green" : "red"}
-        link={`/waiver/${props.player.id}`}
-        last={true}
-      />
-    </ol>
+    <div class="my-8">
+      <div class="flex justify-between my-4">
+        <span class="text-base font-medium dark:text-white text-center">
+          <div class="text-4xl font-bold text-gray-900 dark:text-white">
+            {percent}%
+          </div>
+          of the profile is complete
+        </span>
+        <div class="w-full">
+          <Switch>
+            <Match when={percent < 100}>
+              <p>Complete the profile to participate in UPAI events</p>
+            </Match>
+            <Match when={percent === 100}>
+              <p>Profile complete! 🎉</p>
+            </Match>
+          </Switch>
+          <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+            <div
+              class={clsx(
+                percent < 100 ? "bg-red-400" : "bg-green-600",
+                "h-2.5 rounded-full"
+              )}
+              style={`width: ${percent}%`}
+            />
+          </div>
+        </div>
+      </div>
+      <ol class="items-center w-full sm:flex sm:space-x-8">
+        <Step
+          title="Profile Info"
+          link="/"
+          icon={identification}
+          color={props?.player ? "green" : "red"}
+        />
+        <Step
+          title="Vaccine Info"
+          icon={props?.player?.vaccination ? shieldCheck : shieldExclamation}
+          link={`/vaccination/${props.player.id}`}
+          color={props?.player?.vaccination ? "green" : "red"}
+        />
+        <Step
+          title="Ultimate Central profile"
+          icon={arrowTopRightOnSquare}
+          link={`/uc-login/${props.player.id}`}
+          color={props?.player?.ultimate_central_id ? "green" : "red"}
+        />
+        <Step
+          title="Membership info"
+          icon={currencyRupee}
+          link={`/membership/${props.player.id}`}
+          color={props?.player?.membership?.is_active ? "green" : "red"}
+        />
+        <Step
+          title="Liability Waiver"
+          icon={
+            props?.player?.membership?.waiver_valid
+              ? handThumbUp
+              : handThumbDown
+          }
+          color={props?.player?.membership?.waiver_valid ? "green" : "red"}
+          link={`/waiver/${props.player.id}`}
+          last={true}
+        />
+      </ol>
+    </div>
   );
 };
 
