82c1fc737f0c734bbd13b54f17542993e24e92ab
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-29 09:42:09 +0530

    player: Remove wording that implies vaccination is mandatory

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -171,10 +171,7 @@ const Player = props => {
                 Vaccination details
               </th>
               <td class="px-6 py-4">
-                <p class="mb-4">
-                  You need to update your vaccination details to participate in
-                  UPAI tournaments.
-                </p>
+                <p class="mb-4">Your vaccination information is not complete</p>
                 <button
                   type="submit"
                   class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
