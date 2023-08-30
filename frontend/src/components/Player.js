512a231023183191eb71e8130156fc3cb2b361d8
Tejaa Chintaluri <saitejaac@gmail.com>
2023-08-30 12:19:49 +0530

    player: Add missing fields in player information
    Closes #59

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -1,10 +1,10 @@
-import { Show, Switch, Match } from "solid-js";
+import { Show, Switch, Match, For } from "solid-js";
 import { useNavigate } from "@solidjs/router";
 import { displayDate } from "../utils";
 import VaccinationInformation from "./VaccinationInformation";
 import StatusStepper from "./StatusStepper";
 import { getLabel } from "../utils";
-import { stateChoices } from "../constants";
+import { genderChoices, occupationChoices, stateChoices } from "../constants";
 
 const Player = props => {
   const navigate = useNavigate();
@@ -24,6 +24,11 @@ const Player = props => {
     navigate(`/vaccination/${playerId}`);
   };
 
+  const navUltimateCentral = (e, playerId) => {
+    e.preventDefault();
+    navigate(`/uc-login/${playerId}`);
+  };
+
   return (
     <div class="relative overflow-x-auto">
       <StatusStepper player={props.player} />
@@ -39,6 +44,24 @@ const Player = props => {
           </tr>
         </thead>
         <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Phone number
+            </th>
+            <td class="px-6 py-4">{props.player?.phone}</td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Email
+            </th>
+            <td class="px-6 py-4">{props.player?.email}</td>
+          </tr>
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <th
               scope="row"
@@ -65,12 +88,91 @@ const Player = props => {
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
             >
+              Teams
+            </th>
+            <td class="px-6 py-4">
+              <Switch>
+                <Match when={!props.player?.ultimate_central_id}>
+                  <p class="mb-4">
+                    Player's Ultimate Central profile is not linked
+                  </p>
+                  <button
+                    type="submit"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    onClick={e => navUltimateCentral(e, props.player?.id)}
+                  >
+                    Link profile
+                  </button>
+                </Match>
+                <Match when={!props.player?.teams?.length}>
+                  <p>No teams associated with player.</p>
+                </Match>
+                <Match when={props.player?.teams}>
+                  <For each={props.player?.teams}>
+                    {team => (
+                      <a
+                        href={`https://indiaultimate.org/en_in/t/${team.ultimate_central_slug}`}
+                        class="bg-blue-100 hover:bg-blue-200 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 inline-flex items-center justify-center"
+                      >
+                        {team.name}
+                      </a>
+                    )}
+                  </For>
+                </Match>
+              </Switch>
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
               State
             </th>
             <td class="px-6 py-4">
               {getLabel(stateChoices, props.player?.state_ut)}
             </td>
           </tr>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Gender
+            </th>
+            <Show
+              when={props.player?.gender != "O"}
+              fallback={<td class="px-6 py-4">{props.player?.other_gender}</td>}
+            >
+              <td class="px-6 py-4">
+                {getLabel(genderChoices, props.player?.gender)}
+              </td>
+            </Show>
+          </tr>
+
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Occupation
+            </th>
+            <td class="px-6 py-4">
+              {getLabel(occupationChoices, props.player?.occupation)}
+            </td>
+          </tr>
+
+          <Show when={props.player?.occupation === "Student"}>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Educational Institution
+              </th>
+              <td class="px-6 py-4">{props.player?.educational_institution}</td>
+            </tr>
+          </Show>
           <Show when={props.player?.membership}>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th
