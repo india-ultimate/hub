5396902c7592bd4fb6d2412d77bb60ef5d128ada
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-31 11:58:10 +0530

    player: Remove membership number from the UI

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -189,17 +189,6 @@ const Player = props => {
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Membership number
-              </th>
-              <td class="px-6 py-4">
-                {props.player?.membership.membership_number}
-              </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
                 Membership validity
               </th>
               <td class="px-6 py-4">
