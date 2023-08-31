677b04b06b68ee346f8190380681252f49719d34
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-08-31 08:43:46 +0530

    db: Use Postgres in production if DATABASE_URL envvar is set

--- hub/production.py
+++ hub/production.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+import dj_database_url
+
 from hub.settings import *  # noqa: F403
 
 DEBUG = False
@@ -10,5 +12,8 @@ SECURE_SSL_REDIRECT = bool(int(os.environ.get("SECURE_SSL_REDIRECT", "1")))
 SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)  # noqa: F405
 
 DATA_DIR = Path("/data")
-DATABASES["default"]["NAME"] = DATA_DIR / "production.db.sqlite"  # noqa: F405
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config()  # noqa: F405
+else:
+    DATABASES["default"]["NAME"] = DATA_DIR / "production.db.sqlite"  # noqa: F405
 MEDIA_ROOT = DATA_DIR / "media"
