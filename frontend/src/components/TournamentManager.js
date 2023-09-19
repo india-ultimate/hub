6ed3f0fb975b95aa0b1f935645d5a68dd34ca881
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-09-19 12:24:10 +0530

    tournament-manager: Add UI for get and create cross pool

--- frontend/src/components/TournamentManager.js
+++ frontend/src/components/TournamentManager.js
@@ -5,9 +5,11 @@ import {
   useQueryClient
 } from "@tanstack/solid-query";
 import {
+  createCrossPool,
   createPool,
   createTournament,
   deleteTournament,
+  fetchCrossPool,
   fetchEvents,
   fetchPools,
   fetchTeams,
@@ -55,6 +57,10 @@ const TournamentManager = () => {
     () => ["pools", selectedTournament()],
     () => fetchPools(selectedTournament())
   );
+  const crossPoolQuery = createQuery(
+    () => ["cross-pool", selectedTournament()],
+    () => fetchCrossPool(selectedTournament())
+  );
 
   const createTournamentMutation = createMutation({
     mutationFn: createTournament,
@@ -79,6 +85,14 @@ const TournamentManager = () => {
     onSuccess: () =>
       queryClient.invalidateQueries({
         queryKey: ["pools", selectedTournament()]
+      })
+  });
+
+  const createCrossPoolMutation = createMutation({
+    mutationFn: createCrossPool,
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: ["cross-pool", selectedTournament()]
       })
   });
 
@@ -339,6 +353,26 @@ const TournamentManager = () => {
                 </button>
               </div>
             </div>
+          </Show>
+        </div>
+        <div>
+          <h3>Cross Pool</h3>
+          <Show
+            when={crossPoolQuery.data?.message}
+            fallback={<p>Cross Pool is Enabled for this Tournament</p>}
+          >
+            <p>{crossPoolQuery.data?.message}</p>
+            <button
+              type="button"
+              onClick={() =>
+                createCrossPoolMutation.mutate({
+                  tournament_id: selectedTournament()
+                })
+              }
+              class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700"
+            >
+              Create Cross Pool
+            </button>
           </Show>
         </div>
       </div>
