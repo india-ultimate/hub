5f08e12946ff0f6f1a12d56ad855c62779f17719
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-09-20 15:23:09 +0530

    vaccination: Add command to append random string to cert filenames

---
+++ server/management/commands/vaccine_certificate_append_hash.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import Any
+
+from django.core.management.base import BaseCommand
+from django.utils.crypto import get_random_string
+from server.models import Vaccination
+
+
+class Command(BaseCommand):
+    help = "Append hash to vaccination filenames"
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        for vaccination in Vaccination.objects.exclude(certificate=""):
+            path = Path(vaccination.certificate.name)
+            abspath = Path(vaccination.certificate.path)
+            new_name = f"{path.stem}-{get_random_string(12)}{path.suffix}"
+            abspath.rename(abspath.with_name(new_name))
+            vaccination.certificate = str(path.with_name(new_name))
+            vaccination.save(update_fields=["certificate"])
