f56c10ffa0148af2e294d7fcbadd62dbe39c037e
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-10-05 18:28:41 +0530

    scripts: Add script to create a local dev user

---
+++ scripts/create-user.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""Script to create a dev user, locally."""
+
+import os
+
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hub.settings")
+django.setup()
+
+from server.models import User
+
+DEV_EMAIL = "developer@example.com"
+
+
+def create_dev_user(email: str, staff: bool, superuser: bool) -> None:
+    first_name = "John"
+    last_name = "Doe"
+
+    user, created = User.objects.get_or_create(
+        email=email,
+        defaults={
+            "first_name": first_name,
+            "last_name": last_name,
+            "username": email,
+            "is_staff": staff,
+            "is_superuser": superuser,
+        },
+    )
+    if not created:
+        user.first_name = first_name
+        user.last_name = last_name
+        user.username = email
+        user.is_staff = staff
+        user.is_superuser = superuser
+
+    user.set_password("password")
+    user.save()
+
+    print(f"Created user: {user}")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--email", default=DEV_EMAIL)
+    parser.add_argument("--not-staff", default=False, action="store_true")
+    parser.add_argument("--not-superuser", default=False, action="store_true")
+
+    args = parser.parse_args()
+    create_dev_user(args.email, not args.not_staff, not args.not_superuser)
