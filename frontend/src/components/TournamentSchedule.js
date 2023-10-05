4c723517273cfe4a39740ba3795773eb2046636d
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-10-05 23:53:52 +0530

    match: Add UI to add video_url and view it

--- frontend/src/components/TournamentSchedule.js
+++ frontend/src/components/TournamentSchedule.js
@@ -19,6 +19,8 @@ import { trophy } from "solid-heroicons/solid";
 import { initFlowbite } from "flowbite";
 import MatchCard from "./tournament/MatchCard";
 import { createStore, reconcile } from "solid-js/store";
+import { Icon } from "solid-heroicons";
+import { play } from "solid-heroicons/solid";
 
 const TournamentSchedule = () => {
   const params = useParams();
@@ -381,6 +383,21 @@ const TournamentSchedule = () => {
                             }
                           )}
                       </p>
+                      <Show when={match.video_url}>
+                        <a
+                          class="flex justify-center"
+                          href={match.video_url}
+                          target="_blank"
+                        >
+                          <button
+                            type="button"
+                            class="text-white mt-2 bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                          >
+                            <Icon class="w-4 mr-2" path={play} />
+                            Watch
+                          </button>
+                        </a>
+                      </Show>
                     </div>
                   </Show>
                 )}
