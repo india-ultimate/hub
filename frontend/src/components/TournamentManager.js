6eebd14d8c337d905a05929811187a2b5c99399a
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-10-06 07:57:53 +0530

    tournament-manager: Add UI for adding spirit score

--- frontend/src/components/TournamentManager.js
+++ frontend/src/components/TournamentManager.js
@@ -36,6 +36,7 @@ import {
 import { createStore, reconcile } from "solid-js/store";
 import MatchCard from "./tournament/MatchCard";
 import CreateTournamentForm from "./tournament/CreateTournamentForm";
+import UpdateSpiritScoreForm from "./tournament/UpdateSpiritScoreForm";
 
 const TournamentManager = () => {
   const queryClient = useQueryClient();
@@ -1092,6 +1093,66 @@ const TournamentManager = () => {
                           </Match>
                           <Match when={match.status === "COM"}>
                             {match.score_team_1 + " - " + match.score_team_2}
+                            <Show
+                              when={
+                                match.spirit_score_team_1 &&
+                                match.spirit_score_team_2
+                              }
+                            >
+                              <p class="text-blue-600 dark:text-blue-500">
+                                Spirit scores added
+                              </p>
+                            </Show>
+
+                            <button
+                              data-modal-target={`spiritModal-${match.id}`}
+                              data-modal-toggle={`spiritModal-${match.id}`}
+                              class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                              type="button"
+                            >
+                              Add Spirit Score
+                            </button>
+                            <div
+                              id={`spiritModal-${match.id}`}
+                              tabIndex="-1"
+                              aria-hidden="true"
+                              class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+                            >
+                              <div class="relative w-full max-w-2xl max-h-full">
+                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                  <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                      Add Spirit Score
+                                    </h3>
+                                    <button
+                                      type="button"
+                                      class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                      data-modal-hide={`spiritModal-${match.id}`}
+                                    >
+                                      <svg
+                                        class="w-3 h-3"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 14 14"
+                                      >
+                                        <path
+                                          stroke="currentColor"
+                                          stroke-linecap="round"
+                                          stroke-linejoin="round"
+                                          stroke-width="2"
+                                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                        />
+                                      </svg>
+                                      <span class="sr-only">Close modal</span>
+                                    </button>
+                                  </div>
+                                  <div class="p-6 space-y-6">
+                                    <UpdateSpiritScoreForm match={match} />
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
                           </Match>
                         </Switch>
                       </td>
---
+++ frontend/src/components/tournament/UpdateSpiritScoreForm.js
@@ -0,0 +1,340 @@
+import { createForm, required } from "@modular-forms/solid";
+import { createSignal, Show } from "solid-js";
+import TextInput from "../TextInput";
+import Select from "../Select";
+import {
+  createMutation,
+  createQuery
+} from "@tanstack/solid-query";
+import { fetchTournamentTeamBySlug, updateMatch } from "../../queries";
+import { initFlowbite } from "flowbite";
+
+const UpdateSpiritScoreForm = props => {
+  const rosterQueryTeam1 = createQuery(
+    () => [
+      "tournament-roster",
+      props.match?.tournament?.event?.ultimate_central_slug,
+      props.match?.team_1?.ultimate_central_slug
+    ],
+    () =>
+      fetchTournamentTeamBySlug(
+        props.match?.tournament?.event?.ultimate_central_slug,
+        props.match?.team_1?.ultimate_central_slug
+      )
+  );
+  const rosterQueryTeam2 = createQuery(
+    () => [
+      "tournament-roster",
+      props.match?.tournament?.event?.ultimate_central_slug,
+      props.match?.team_2?.ultimate_central_slug
+    ],
+    () =>
+      fetchTournamentTeamBySlug(
+        props.match?.tournament?.event?.ultimate_central_slug,
+        props.match?.team_2?.ultimate_central_slug
+      )
+  );
+
+  const initialValues = {
+    spirit_score_team_1: {
+      rules: 2,
+      fouls: 2,
+      fair: 2,
+      positive: 2,
+      communication: 2
+    },
+    spirit_score_team_2: {
+      rules: 2,
+      fouls: 2,
+      fair: 2,
+      positive: 2,
+      communication: 2
+    }
+  };
+  const [_tournamentForm, { Form, Field }] = createForm({
+    initialValues,
+    validateOn: "touched",
+    revalidateOn: "touched"
+  });
+
+  const [status, setStatus] = createSignal("");
+  const [error, setError] = createSignal("");
+
+  const updateSpiritScoreMutation = createMutation({
+    mutationFn: updateMatch,
+    onSuccess: () => {
+      setStatus("Updated Score!");
+    },
+    onError: e => {
+      console.log(e);
+      setError(e);
+    }
+  });
+
+  const handleSubmit = async values => {
+    setStatus("");
+    setError("");
+
+    updateSpiritScoreMutation.mutate({
+      match_id: props?.match?.id,
+      body: values
+    });
+
+    setTimeout(() => initFlowbite(), 1000);
+  };
+
+  return (
+    <div>
+      <Form
+        class="mt-12 space-y-12 md:space-y-14 lg:space-y-16"
+        onSubmit={values => handleSubmit(values)}
+      >
+        <div class="space-y-8">
+          <h2 class="text-2xl text-bold text-blue-600 dark:text-blue-500 text-center">
+            {props?.match?.team_1?.name}
+          </h2>
+          <Field
+            name="spirit_score_team_1.rules"
+            validate={required("Please add rules score.")}
+          >
+            {(field, props) => (
+              <TextInput
+                {...props}
+                value={field.value}
+                error={field.error}
+                type="number"
+                label="Rules Score"
+                required
+              />
+            )}
+          </Field>
+          <Field
+            name="spirit_score_team_1.fouls"
+            validate={required("Please add fouls score.")}
+          >
+            {(field, props) => (
+              <TextInput
+                {...props}
+                value={field.value}
+                error={field.error}
+                type="number"
+                label="Fouls Score"
+                required
+              />
+            )}
+          </Field>
+          <Field
+            name="spirit_score_team_1.fair"
+            validate={required("Please add fair score.")}
+          >
+            {(field, props) => (
+              <TextInput
+                {...props}
+                value={field.value}
+                error={field.error}
+                type="number"
+                label="Fair Score"
+                required
+              />
+            )}
+          </Field>
+          <Field
+            name="spirit_score_team_1.positive"
+            validate={required("Please add positive score.")}
+          >
+            {(field, props) => (
+              <TextInput
+                {...props}
+                value={field.value}
+                error={field.error}
+                type="number"
+                label="Positive Score"
+                required
+              />
+            )}
+          </Field>
+          <Field
+            name="spirit_score_team_1.communication"
+            validate={required("Please add communication score.")}
+          >
+            {(field, props) => (
+              <TextInput
+                {...props}
+                value={field.value}
+                error={field.error}
+                type="number"
+                label="Communication Score"
+                required
+              />
+            )}
+          </Field>
+          <Field name="spirit_score_team_1.mvp_id">
+            {(field, fieldProps) => (
+              <Select
+                {...fieldProps}
+                value={field.value}
+                error={field.error}
+                options={rosterQueryTeam1.data?.map(r => {
+                  return {
+                    value: r.id.toString(),
+                    label: r.first_name + r.last_name
+                  };
+                })}
+                type="text"
+                label="MVP"
+                placeholder="Choose a Player"
+              />
+            )}
+          </Field>
+          <Field name="spirit_score_team_1.msp_id">
+            {(field, fieldProps) => (
+              <Select
+                {...fieldProps}
+                value={field.value}
+                error={field.error}
+                options={rosterQueryTeam1.data?.map(r => {
+                  return {
+                    value: r.id.toString(),
+                    label: r.first_name + r.last_name
+                  };
+                })}
+                type="text"
+                label="MSP"
+                placeholder="Choose a Player"
+              />
+            )}
+          </Field>
+
+          <h2 class="text-2xl text-bold text-blue-600 dark:text-blue-500 text-center">
+            {props?.match?.team_2?.name}
+          </h2>
+          <Field
+            name="spirit_score_team_2.rules"
+            validate={required("Please add rules score.")}
+          >
+            {(field, props) => (
+              <TextInput
+                {...props}
+                value={field.value}
+                error={field.error}
+                type="number"
+                label="Rules Score"
+                required
+              />
+            )}
+          </Field>
+          <Field
+            name="spirit_score_team_2.fouls"
+            validate={required("Please add fouls score.")}
+          >
+            {(field, props) => (
+              <TextInput
+                {...props}
+                value={field.value}
+                error={field.error}
+                type="number"
+                label="Fouls Score"
+                required
+              />
+            )}
+          </Field>
+          <Field
+            name="spirit_score_team_2.fair"
+            validate={required("Please add fair score.")}
+          >
+            {(field, props) => (
+              <TextInput
+                {...props}
+                value={field.value}
+                error={field.error}
+                type="number"
+                label="Fair Score"
+                required
+              />
+            )}
+          </Field>
+          <Field
+            name="spirit_score_team_2.positive"
+            validate={required("Please add positive score.")}
+          >
+            {(field, props) => (
+              <TextInput
+                {...props}
+                value={field.value}
+                error={field.error}
+                type="number"
+                label="Positive Score"
+                required
+              />
+            )}
+          </Field>
+          <Field
+            name="spirit_score_team_2.communication"
+            validate={required("Please add communication score.")}
+          >
+            {(field, props) => (
+              <TextInput
+                {...props}
+                value={field.value}
+                error={field.error}
+                type="number"
+                label="Communication Score"
+                required
+              />
+            )}
+          </Field>
+          <Field name="spirit_score_team_2.mvp_id">
+            {(field, fieldProps) => (
+              <Select
+                {...fieldProps}
+                value={field.value}
+                error={field.error}
+                options={rosterQueryTeam2.data?.map(r => {
+                  return {
+                    value: r.id.toString(),
+                    label: r.first_name + r.last_name
+                  };
+                })}
+                type="text"
+                label="MVP"
+                placeholder="Choose a Player"
+              />
+            )}
+          </Field>
+          <Field name="spirit_score_team_2.msp_id">
+            {(field, fieldProps) => (
+              <Select
+                {...fieldProps}
+                value={field.value}
+                error={field.error}
+                options={rosterQueryTeam2.data?.map(r => {
+                  return {
+                    value: r.id.toString(),
+                    label: r.first_name + r.last_name
+                  };
+                })}
+                type="text"
+                label="MSP"
+                placeholder="Choose a Player"
+              />
+            )}
+          </Field>
+          <button
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+          <Show when={error()}>
+            <p class="my-2 text-sm text-red-600 dark:text-red-500">
+              <span class="font-medium">Oops!</span> {error()}
+            </p>
+          </Show>
+          <p>{status()}</p>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default UpdateSpiritScoreForm;
