8c10436f95bfb154ad8d058a948b92aebb8deb40
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-10-06 07:57:53 +0530

    tournament-manager: Add UI for adding spirit score

--- frontend/src/queries.js
+++ frontend/src/queries.js
@@ -419,3 +419,23 @@ export const addMatchScore = async ({ match_id, body }) => {
   });
   return await response.json();
 };
+
+export const addMatchSpiritScore = async ({ match_id, body }) => {
+  const response = await fetch(`/api/match/${match_id}/update`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": getCookie("csrftoken")
+    },
+    credentials: "same-origin",
+    body: JSON.stringify(body)
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data?.message || JSON.stringify(data));
+  }
+
+  return data;
+};
