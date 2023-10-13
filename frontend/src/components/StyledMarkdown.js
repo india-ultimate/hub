b46e9dbb68a6a26895ef11bf4dbe606ff7f1b2db
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-10-13 16:07:51 +0530

    help: Use help.md and StyledMarkdown component to generate help
    Move content from HelpImport to help.md

--- frontend/src/components/StyledMarkdown.js
+++ frontend/src/components/StyledMarkdown.js
@@ -1,20 +1,29 @@
 import SolidMarkdown from "solid-markdown";
-import { onMount, createEffect } from "solid-js";
+import { onMount } from "solid-js";
+import slug from "slug";
 
 const TailwindMarkdown = props => {
   let divRef;
 
   const classMap = {
     a: "font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline",
-    p: "mb-2"
+    p: "mb-2",
+    h1: "text-2xl font-bold mb-4 text-blue-500",
+    ol: "m-4 text-gray-500 list-decimal list-outside dark:text-gray-400"
   };
 
   onMount(() => {
+    // Add CSS classes
     for (let tag in classMap) {
       divRef.querySelectorAll(tag).forEach(el => {
         el.setAttribute("class", classMap[tag]);
       });
     }
+
+    // Add ids to h1 tags
+    divRef.querySelectorAll("h1").forEach(el => {
+      el.setAttribute("id", slug(el.innerText));
+    });
   });
 
   return (
---
+++ frontend/src/help.md
@@ -0,0 +1,11 @@
+# Import Players from a Spreadsheet
+
+For admins who create and manage the profiles of multiple players of their
+team, we provide a way to create user accounts and import player profiles from
+a Spreadsheet.
+
+1. Create a copy of the template [here](https://docs.google.com/spreadsheets/d/1UGSlRHqRA8V_Extze5mh2Y7TzmQlJjqZkwpXb2Osvck)
+
+1. Fill up the details of all the players of your team
+
+1. Reach out to the India Ultimate Tech team via [WhatsApp](https://chat.whatsapp.com/L1LkrLGuqFdIiRh5kuuAnz)
