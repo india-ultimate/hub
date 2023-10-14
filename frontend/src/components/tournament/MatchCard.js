a0e95d83fd3d5a62b43d0bf4662d12c6dd7df0f7
AnishKacham <f20190091@hyderabad.bits-pilani.ac.in>
2023-10-14 23:47:30 +0530

    schedule: Scroll to detailed card on match card click

--- frontend/src/components/tournament/MatchCard.js
+++ frontend/src/components/tournament/MatchCard.js
@@ -32,67 +32,90 @@ const MatchCard = props => {
     }
   });
 
+  const handleMatchCardClick = matchId => {
+    const resultCard = document.getElementById(matchId);
+    if (resultCard) {
+      resultCard.scrollIntoView({ behavior: "smooth" });
+      resultCard;
+    }
+  };
+
+  const renderBadge = () => {
+    return (
+      <Switch>
+        <Match when={props.match.pool}>
+          <div
+            class={`flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
+          >
+            <h6 class="text-center w-full">Pool {props.match.pool.name}</h6>
+            <Show when={props.showSeed}>
+              <h6 class="text-center">
+                {props.match.placeholder_seed_1 +
+                  " vs " +
+                  props.match.placeholder_seed_2}
+              </h6>
+            </Show>
+          </div>
+        </Match>
+        <Match when={props.match.cross_pool}>
+          <div
+            class={`flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
+          >
+            <h6 class="text-center w-full">CP</h6>
+            <Show when={props.showSeed}>
+              <h6 class="text-center">
+                {props.match.placeholder_seed_1 +
+                  " vs " +
+                  props.match.placeholder_seed_2}
+              </h6>
+            </Show>
+          </div>
+        </Match>
+        <Match when={props.match.bracket}>
+          <div
+            class={`flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
+          >
+            <h6 class="text-center w-full">B {props.match.bracket.name}</h6>
+            <Show when={props.showSeed}>
+              <h6 class="text-center">
+                {props.match.placeholder_seed_1 +
+                  " vs " +
+                  props.match.placeholder_seed_2}
+              </h6>
+            </Show>
+          </div>
+        </Match>
+        <Match when={props.match.position_pool}>
+          <div
+            class={`flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
+          >
+            <h6 class="text-center w-full">
+              Pos. Pool {props.match.position_pool.name}
+            </h6>
+            <Show when={props.showSeed}>
+              <h6 class="text-center">
+                {props.match.placeholder_seed_1 +
+                  " vs " +
+                  props.match.placeholder_seed_2}
+              </h6>
+            </Show>
+          </div>
+        </Match>
+      </Switch>
+    );
+  };
+
   return (
-    <Switch>
-      <Match when={props.match.pool}>
-        <div
-          class={`flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
-        >
-          <h6 class="text-center w-full">Pool {props.match.pool.name}</h6>
-          <Show when={props.showSeed}>
-            <h6 class="text-center">
-              {props.match.placeholder_seed_1 +
-                " vs " +
-                props.match.placeholder_seed_2}
-            </h6>
-          </Show>
-        </div>
-      </Match>
-      <Match when={props.match.cross_pool}>
-        <div
-          class={`flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
-        >
-          <h6 class="text-center w-full">CP</h6>
-          <Show when={props.showSeed}>
-            <h6 class="text-center">
-              {props.match.placeholder_seed_1 +
-                " vs " +
-                props.match.placeholder_seed_2}
-            </h6>
-          </Show>
-        </div>
-      </Match>
-      <Match when={props.match.bracket}>
-        <div
-          class={`flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
-        >
-          <h6 class="text-center w-full">B {props.match.bracket.name}</h6>
-          <Show when={props.showSeed}>
-            <h6 class="text-center">
-              {props.match.placeholder_seed_1 +
-                " vs " +
-                props.match.placeholder_seed_2}
-            </h6>
-          </Show>
-        </div>
-      </Match>
-      <Match when={props.match.position_pool}>
-        <div
-          class={`flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
-        >
-          <h6 class="text-center w-full">
-            Pos. Pool {props.match.position_pool.name}
-          </h6>
-          <Show when={props.showSeed}>
-            <h6 class="text-center">
-              {props.match.placeholder_seed_1 +
-                " vs " +
-                props.match.placeholder_seed_2}
-            </h6>
-          </Show>
-        </div>
-      </Match>
-    </Switch>
+    <Show when={props.setFlash} fallback={renderBadge()}>
+      <button
+        class="w-full"
+        onClick={() => {
+          handleMatchCardClick(props.match.id);
+        }}
+      >
+        {renderBadge()}
+      </button>
+    </Show>
   );
 };
 
