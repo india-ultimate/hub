bc0a390f9f668119ce25c9b7ce30acbd6820fa3e
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-10-22 22:04:28 +0530

    tests: Add UI tests for OTP login

--- server/tests/utils.py
+++ server/tests/utils.py
@@ -1,4 +1,6 @@
 import re
+import shutil
+from pathlib import Path
 from typing import Any
 
 import zulip
@@ -27,3 +29,24 @@ def _find_sign_up_link(message: dict[str, Any]) -> None:
         link = match.group(1)
         raise EmailLinkFound(link)
     return
+
+
+def create_empty_directory(directory_path: Path) -> None:
+    shutil.rmtree(directory_path, ignore_errors=True)
+    directory_path.mkdir(exist_ok=True)
+
+
+def get_otp_from_email_logs(directory_path: Path) -> str:
+    files = list(directory_path.glob("*"))
+
+    if not files:
+        raise ValueError("File not found")
+
+    html_content = files[0].read_text()
+
+    # Use regular expression to find content between <strong> tags
+    strong_contents = re.findall(
+        r'<strong style="font-size: 130%">(.*?)</strong>', html_content, re.DOTALL
+    )
+
+    return strong_contents[0]
