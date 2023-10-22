d0fa67f33988c2e07df7fc6188fa3316822e57c2
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-10-22 14:43:21 +0530

    otp-login: Add a security check for OTP_EMAIL_HASH_KEY setting

--- server/apps.py
+++ server/apps.py
@@ -1,4 +1,23 @@
+from typing import Any
+
 from django.apps import AppConfig
+from django.conf import settings
+from django.core.checks import Error, Tags, register
+
+
+@register(Tags.security, deploy=True)
+def enforce_otp_email_hash(app_configs: Any, **kwargs: Any) -> list[Error]:
+    errors = []
+    if not settings.OTP_EMAIL_HASH_KEY:
+        errors.append(
+            Error(
+                "OTP_EMAIL_HASH_KEY needs to be set",
+                hint="Set the environment variable to be non-empty.",
+                obj=settings,
+                id="server.E001",
+            )
+        )
+    return errors
 
 
 class ServerConfig(AppConfig):
