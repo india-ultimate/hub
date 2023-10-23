5dc83ee888b1a0e207c1888980c86226e8822447
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-10-23 23:12:43 +0530

    spinner: Make it take default text color from parent

--- frontend/src/icons.js
+++ frontend/src/icons.js
@@ -9,7 +9,7 @@ export const Spinner = () => (
   <>
     <svg
       aria-hidden="true"
-      class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+      class="inline w-8 h-8 mr-2 animate-spin fill-blue-600"
       role="status"
       viewBox="0 0 100 101"
       fill="none"
