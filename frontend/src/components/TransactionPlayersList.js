fea33d3a55bdd6beee40f2d14ee32c0064ea7173
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-10-27 23:25:33 +0530

    transaction: Show player count if # of players is greater than 1
    Closes #140

--- frontend/src/components/TransactionPlayersList.js
+++ frontend/src/components/TransactionPlayersList.js
@@ -2,15 +2,18 @@ import { For } from "solid-js";
 
 const PlayersList = props => {
   return (
-    <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-      <For each={props.players}>
-        {player_name => (
-          <li class="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-            {player_name}
-          </li>
-        )}
-      </For>
-    </ul>
+    <>
+      <p class="p-2 text-xs">Payment for {props.players.length} players</p>
+      <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        <For each={props.players}>
+          {player_name => (
+            <li class="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+              {player_name}
+            </li>
+          )}
+        </For>
+      </ul>
+    </>
   );
 };
 
