ca73486006d91c5351012b8598e395b6dfddc231
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-10-30 14:26:00 +0100

    deploy: Add a check to ensure proper PhonePe env vars usage

--- server/apps.py
+++ server/apps.py
@@ -1,8 +1,9 @@
+from collections.abc import Sequence
 from typing import Any
 
 from django.apps import AppConfig
 from django.conf import settings
-from django.core.checks import Error, Tags, register
+from django.core.checks import Error, Tags, Warning, register
 
 
 @register(Tags.security, deploy=True)
@@ -17,6 +18,33 @@ def enforce_otp_email_hash(app_configs: Any, **kwargs: Any) -> list[Error]:
                 id="server.E001",
             )
         )
+    return errors
+
+
+@register(Tags.security, deploy=True)
+def check_phonepe_env_vars(app_configs: Any, **kwargs: Any) -> Sequence[Error | Warning]:
+    errors = []
+    if not settings.PHONEPE_MERCHANT_ID:
+        return [Warning("PHONEPE_MERCHANT_ID not set", id="settings.W001", obj=settings)]
+    if settings.PHONEPE_PRODUCTION and settings.PHONEPE_MERCHANT_ID.endswith("UAT"):
+        errors.append(
+            Error(
+                "Use production PhonePe MERCHANT_ID",
+                hint="Set the correct values for PHONEPE_MERCHANT_ID and PHONEPE_PRODUCTION.",
+                obj=settings,
+                id="server.E002",
+            )
+        )
+    elif not settings.PHONEPE_PRODUCTION and not settings.PHONEPE_MERCHANT_ID.endswith("UAT"):
+        errors.append(
+            Error(
+                "Use UAT PhonePe MERCHANT_ID",
+                hint="Set the correct values for PHONEPE_MERCHANT_ID and PHONEPE_PRODUCTION.",
+                obj=settings,
+                id="server.E003",
+            )
+        )
+
     return errors
 
 
