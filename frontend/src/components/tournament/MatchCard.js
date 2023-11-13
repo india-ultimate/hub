1e13928820b8f94445ab2aac69d2021627ffb53f
SibiAkkash <sibi.akkash@gmail.com>
2023-11-13 10:52:13 +0530

    tournament-matches: Show generated match name in match card
    TournamentMatch can use full match name on smaller screens, as we have space in
    the match block.  Added a dontMinimise prop to MatchCard for this.

--- frontend/src/components/tournament/MatchCard.js
+++ frontend/src/components/tournament/MatchCard.js
@@ -11,6 +11,62 @@ const colors = {
   position_pool: ["lime", "pink", "emerald"]
 };
 
+const getSmallName = matchName => {
+  if (matchName === "Cross Pool") {
+    return "CP";
+  }
+  if (matchName === "Quarter Finals") {
+    return "QF";
+  }
+  if (matchName === "Semi Finals") {
+    return "SF";
+  }
+  // Bracket matches are named like 5-8 Bracket, 9-12 Bracket
+  // On smaller screens show as B 5-8, B 9-12
+  const matchBracketPattern = matchName?.match(/(\d+-\d+) Bracket/);
+  if (matchBracketPattern) {
+    return `B ${matchBracketPattern[1]}`;
+  }
+  // Return same name otherwise (Finals)
+  return matchName;
+};
+
+const showSeedForBracketMatch = matchName => {
+  // Don't show seed for finals or position matches (3rd Place match is 3v4)
+  const isFinals = matchName === "Finals";
+  const isPositionMatch = matchName?.match(/(\d+)\w{2} Place/) !== null;
+  return !(isFinals || isPositionMatch);
+};
+
+const MatchName = props => (
+  <Show
+    when={props.dontMinimiseMatchName}
+    fallback={
+      <>
+        <h6
+          class="text-center w-full block sm:hidden"
+          classList={{ "py-2": props.extraVerticalPadding }}
+        >
+          {getSmallName(props.name)}
+        </h6>
+        <h6
+          class="text-center w-full hidden sm:block"
+          classList={{ "py-2": props.extraVerticalPadding }}
+        >
+          {props.name}
+        </h6>
+      </>
+    }
+  >
+    <h6
+      class="text-center w-full"
+      classList={{ "py-2": props.extraVerticalPadding }}
+    >
+      {props.name}
+    </h6>
+  </Show>
+);
+
 const MatchCard = props => {
   const [color, setColor] = createSignal("blue");
 
@@ -48,36 +104,12 @@ const MatchCard = props => {
     }
   };
 
-  const getSmallNameForBracket = matchName => {
-    if (matchName === "Quarter Finals") {
-      return "QF";
-    }
-    if (matchName === "Semi Finals") {
-      return "SF";
-    }
-    // Bracket matches are named like 5-8 Bracket, 9-12 Bracket
-    // On smaller screens show as B 5-8, B 9-12
-    const matchBracketPattern = matchName.match(/(\d+-\d+) Bracket/);
-    if (matchBracketPattern) {
-      return `B ${matchBracketPattern[1]}`;
-    }
-    // Return same name otherwise (Finals)
-    return matchName;
-  };
-
-  const showSeedForBracketMatch = matchName => {
-    // Don't show seed for finals or position matches (3rd Place match is 3v4)
-    const isFinals = matchName === "Finals";
-    const isPositionMatch = matchName?.match(/(\d+)\w{2} Place/) !== null;
-    return !(isFinals || isPositionMatch);
-  };
-
   const renderBadge = () => {
     return (
       <Switch>
         <Match when={props.match.pool || props.match.position_pool}>
           <div
-            class={`w-full flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
+            class={`w-full flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
           >
             <Show
               when={props.showSeed}
@@ -94,40 +126,24 @@ const MatchCard = props => {
             </Show>
           </div>
         </Match>
-        <Match when={props.match.cross_pool}>
+        <Match when={props.match.cross_pool || props.match.bracket}>
           <div
             class={`w-full flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
-          >
-            <h6 class="text-center w-full block sm:hidden">CP</h6>
-            <h6 class="text-center w-full hidden sm:block">
-              {props.match.name}
-            </h6>
-            <Show when={props.showSeed}>
-              <h6 class="text-center">
-                {props.match.placeholder_seed_1 +
-                  " v " +
-                  props.match.placeholder_seed_2}
-              </h6>
-            </Show>
-          </div>
-        </Match>
-
-        <Match when={props.match.bracket}>
-          <div
-            class={`w-full flex justify-center flex-wrap bg-${color()}-100 text-${color()}-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-${color()}-900 dark:text-${color()}-300`}
           >
             <Show
               when={props.showSeed && showSeedForBracketMatch(props.match.name)}
               fallback={
-                <h6 class="text-center w-full py-2">{props.match.name}</h6>
+                <MatchName
+                  name={props.match.name}
+                  dontMinimiseMatchName={props.dontMinimiseMatchName}
+                  extraVerticalPadding
+                />
               }
             >
-              <h6 class="text-center w-full block sm:hidden">
-                {getSmallNameForBracket(props.match.name)}
-              </h6>
-              <h6 class="text-center w-full hidden sm:block">
-                {props.match.name}
-              </h6>
+              <MatchName
+                name={props.match.name}
+                dontMinimiseMatchName={props.dontMinimiseMatchName}
+              />
               <h6 class="text-center">
                 {props.match.placeholder_seed_1 +
                   " v " +
