c1151c686a98e91854c5e8dfa10631a6dcd6d628
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-11-14 17:02:56 +0530

    header: Add tournaments to navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -96,6 +96,16 @@ export default function Header() {
                 </A>
               </li>
               <li>
+                <A
+                  href="/tournaments"
+                  activeClass="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                  inactiveClass="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  end={true}
+                >
+                  Tournaments
+                </A>
+              </li>
+              <li>
                 <Show
                   when={store.loggedIn}
                   fallback={
