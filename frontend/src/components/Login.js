1ec86690567204ca3c543898e95e6e94be2c815f
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-11-16 15:34:23 +0530

    email-otp: Add email type to input

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -217,6 +217,7 @@ const SendEmailOTP = props => {
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           id="email-otp-input"
           placeholder="Email Address"
+          type="email"
           value={email()}
           disabled={otpData()}
           onInput={e => setEmail(e.currentTarget.value)}
