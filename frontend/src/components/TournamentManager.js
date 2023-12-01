2bb671bb284fea899138a75a233c759308c495a0
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-12-01 11:26:15 +0530

    tournament-manager: Add rules markdown editor and preview

--- frontend/src/components/TournamentManager.js
+++ frontend/src/components/TournamentManager.js
@@ -41,6 +41,7 @@ import { useStore } from "../store";
 import CreateTournamentForm from "./tournament/CreateTournamentForm";
 import MatchCard from "./tournament/MatchCard";
 import ReorderTeams from "./tournament/ReorderTeams";
+import RulesMarkdownEditor from "./tournament/RulesMarkdownEditor";
 import UpdateSpiritScoreForm from "./tournament/UpdateSpiritScoreForm";
 
 const TournamentManager = () => {
@@ -428,6 +429,58 @@ const TournamentManager = () => {
             >
               Delete Tournament
             </button>
+
+            <button
+              data-modal-target={`rules-modal-${selectedTournamentID()}`}
+              data-modal-toggle={`rules-modal-${selectedTournamentID()}`}
+              class="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              type="button"
+            >
+              Edit Format & Rules
+            </button>
+
+            <div
+              id={`rules-modal-${selectedTournamentID()}`}
+              data-modal-backdrop="static"
+              tabIndex="-1"
+              aria-hidden="true"
+              class="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden md:inset-0"
+            >
+              <div class="relative max-h-full w-full p-4">
+                <div class="relative rounded-lg bg-white shadow dark:bg-gray-700">
+                  <div class="flex items-center justify-between rounded-t border-b p-4 dark:border-gray-600 md:p-5">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                      Rules Editor & Preview
+                    </h3>
+                    <button
+                      type="button"
+                      class="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+                      data-modal-hide={`rules-modal-${selectedTournamentID()}`}
+                    >
+                      <svg
+                        class="h-3 w-3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 14"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                        />
+                      </svg>
+                      <span class="sr-only">Close modal</span>
+                    </button>
+                  </div>
+                  <div class="space-y-4 p-4 md:p-5">
+                    <RulesMarkdownEditor tournament={selectedTournament()} />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div>
             <h2 class="mb-5 text-xl font-bold text-blue-500">Pools</h2>
