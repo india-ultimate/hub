e4d6b1e35089bb74ba53ab31574f55ad76741f70
SibiAkkash <sibi.akkash@gmail.com>
2023-12-02 10:30:42 +0530

    team-link: Add link to player's team page
    The error was in the Breadcrumbs component,
    due to the usage of the native <a> tag instead of the <A> tag from solid router

--- frontend/src/components/Breadcrumbs.js
+++ frontend/src/components/Breadcrumbs.js
@@ -1,3 +1,4 @@
+import { A } from "@solidjs/router";
 import { Icon } from "solid-heroicons";
 import { chevronRight } from "solid-heroicons/solid";
 import { For, Show } from "solid-js";
@@ -12,13 +13,13 @@ const Breadcrumbs = props => {
               when={i() > 0}
               fallback={
                 <li class="inline-flex items-center">
-                  <a
+                  <A
                     href={page.url}
                     class="inline-flex items-center rounded-full border border-gray-300 bg-white px-3 py-1 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700"
                   >
                     <Icon class="mr-2.5 h-3 w-3" path={props.icon} />
                     {page.name}
-                  </a>
+                  </A>
                 </li>
               }
             >
@@ -36,12 +37,12 @@ const Breadcrumbs = props => {
                       </span>
                     }
                   >
-                    <a
+                    <A
                       href={page.url}
                       class="ml-1 rounded-full border border-gray-300 bg-white px-3 py-1 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700"
                     >
                       {page.name}
-                    </a>
+                    </A>
                   </Show>
                 </div>
               </li>
