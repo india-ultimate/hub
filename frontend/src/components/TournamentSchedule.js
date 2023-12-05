51cdd43c41e1c1ddce91cf868b56dfe1aa8e9072
Jonathan Samuel <jonathansamuel2k@gmail.com>
2023-12-05 22:06:33 +0530

    tournament-schedule: Show alert when no matches present on a day

--- frontend/src/components/TournamentSchedule.js
+++ frontend/src/components/TournamentSchedule.js
@@ -252,6 +252,34 @@ const TournamentSchedule = () => {
                   </Show>
                 )}
               </For>
+              <Show
+                when={
+                  matchesQuery.data?.filter(match =>
+                    sameDay(day, new Date(Date.parse(match.time)))
+                  ).length === 0
+                }
+              >
+                <div
+                  class="mb-4 flex items-center rounded-lg border border-blue-300 bg-blue-50 p-4 text-sm text-blue-800 dark:border-blue-800 dark:bg-gray-800 dark:text-blue-400"
+                  role="alert"
+                >
+                  <svg
+                    class="me-3 inline h-4 w-4 flex-shrink-0"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                  </svg>
+                  <span class="sr-only">Info</span>
+                  <div>
+                    <span class="font-medium capitalize">
+                      No Matches Present on this day!
+                    </span>
+                  </div>
+                </div>
+              </Show>
               <For each={matchesQuery.data}>
                 {match => (
                   <Show when={sameDay(day, new Date(Date.parse(match.time)))}>
