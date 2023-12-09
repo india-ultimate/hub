ae799ecb9b596b4fe581d36a27dfd9e1d83b355c
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-12-09 14:39:14 +0530

    lint: Fix eslint solid/* warnings in StatusStepper

--- frontend/src/components/StatusStepper.js
+++ frontend/src/components/StatusStepper.js
@@ -13,25 +13,35 @@ import {
   shieldExclamation,
   xCircle
 } from "solid-heroicons/solid-mini";
-import { Match, Switch } from "solid-js";
+import { createEffect, createSignal, Match, Switch } from "solid-js";
 
 import { getStatusAndPercent } from "../utils";
 
 const Step = props => {
-  const liClass = clsx(
-    props?.color !== "gray"
-      ? `flex w-full items-center text-${props.color}-600 dark:text-${props.color}-500 space-x-2.5`
-      : "flex w-full items-center space-x-2.5 text-gray-500 dark:text-gray-400"
-  );
-  const iconClass = clsx(
-    "flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0",
-    props?.color && `bg-${props.color}-100 dark:bg-${props.color}-700`,
-    !props?.color && "bg-gray-100 dark:bg-gray-700"
-  );
+  const [li, setLi] = createSignal("");
+  const [icon, setIcon] = createSignal("");
+
+  createEffect(() => {
+    setLi(
+      clsx(
+        props?.color !== "gray"
+          ? `flex w-full items-center text-${props.color}-600 dark:text-${props.color}-500 space-x-2.5`
+          : "flex w-full items-center space-x-2.5 text-gray-500 dark:text-gray-400"
+      )
+    );
+    setIcon(
+      clsx(
+        "flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0",
+        props?.color && `bg-${props.color}-100 dark:bg-${props.color}-700`,
+        !props?.color && "bg-gray-100 dark:bg-gray-700"
+      )
+    );
+  });
+
   return (
-    <li class={liClass}>
+    <li class={li()}>
       <A class="flex items-center" href={props.link || ""}>
-        <span class={iconClass}>
+        <span class={icon()}>
           <Icon path={props.icon} style={{ width: "20px" }} />
         </span>
         <span class="ml-2">
@@ -43,13 +53,19 @@ const Step = props => {
 };
 
 const StatusStepper = props => {
-  const { status, percent } = getStatusAndPercent(props?.player);
+  const [status, setStatus] = createSignal({});
+  const [percent, setPercent] = createSignal(0);
+  createEffect(() => {
+    const { status, percent } = getStatusAndPercent(props?.player);
+    setStatus(status);
+    setPercent(percent);
+  });
   return (
     <div class="my-8">
       <div class="my-4 flex justify-between">
         <span class="text-center text-base font-medium dark:text-white">
           <div class="text-4xl font-bold text-gray-900 dark:text-white">
-            {percent}%
+            {percent()}%
           </div>
           of the profile is complete
         </span>
@@ -57,14 +73,14 @@ const StatusStepper = props => {
           <div class="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
             <div
               class={clsx(
-                percent < 100 ? "bg-red-400" : "bg-green-600",
+                percent() < 100 ? "bg-red-400" : "bg-green-600",
                 "h-2.5 rounded-full"
               )}
-              style={`width: ${percent}%`}
+              style={{ width: `${percent()}%` }}
             />
           </div>
           <Switch>
-            <Match when={percent < 100}>
+            <Match when={percent() < 100}>
               <p class="text-md mt-2">
                 Complete the profile to participate in India Ultimate events
               </p>
@@ -73,7 +89,7 @@ const StatusStepper = props => {
                 to complete those steps.
               </p>
             </Match>
-            <Match when={percent === 100}>
+            <Match when={percent() === 100}>
               <p>Profile complete! 🎉</p>
             </Match>
           </Switch>
@@ -84,42 +100,42 @@ const StatusStepper = props => {
           title="Profile Info"
           link={`/edit/registration/${props.player.id}`}
           icon={identification}
-          color={status.profile ? "green" : "red"}
+          color={status().profile ? "green" : "red"}
         />
         <Step
           title="Vaccine Info"
-          icon={status.vaccine ? shieldCheck : shieldExclamation}
+          icon={status().vaccine ? shieldCheck : shieldExclamation}
           link={`/vaccination/${props.player.id}`}
-          color={status.vaccine ? "green" : "red"}
+          color={status().vaccine ? "green" : "red"}
         />
         <Step
           title="Ultimate Central profile"
           icon={arrowTopRightOnSquare}
           link={`/uc-login/${props.player.id}`}
-          color={status.ucLink ? "green" : "red"}
+          color={status().ucLink ? "green" : "red"}
         />
         <Step
           title="Membership info"
           icon={currencyRupee}
           link={`/membership/${props.player.id}`}
-          color={status.membership ? "green" : "red"}
+          color={status().membership ? "green" : "red"}
         />
         <Step
           title="Liability Waiver"
-          icon={status.waiver ? handThumbUp : handThumbDown}
-          color={status.waiver ? "green" : "red"}
+          icon={status().waiver ? handThumbUp : handThumbDown}
+          color={status().waiver ? "green" : "red"}
           link={`/waiver/${props.player.id}`}
         />
         <Step
           title="Accreditation"
           icon={
-            status.accreditation
+            status().accreditation
               ? props?.player?.accreditation?.level == "ADV"
                 ? documentCheck
                 : document
               : xCircle
           }
-          color={status.accreditation ? "green" : "red"}
+          color={status().accreditation ? "green" : "red"}
           link={`/accreditation/${props.player.id}`}
           last={true}
         />
