8c748832a635a82f3720791cb439d7a3303b310b
Jonathan Samuel <jonathansamuel2k@gmail.com>
2024-03-18 18:24:13 +0530

    home: Add new sections

---
+++ frontend/src/components/home/PlayerSection.js
@@ -0,0 +1,74 @@
+import { A } from "@solidjs/router";
+import { Show } from "solid-js";
+
+import { useStore } from "../../store";
+
+const PlayerSection = () => {
+  const [store] = useStore();
+
+  return (
+    <>
+      <div class="rounded-lg bg-blue-200 md:hidden">
+        <div class="p-4">
+          <img class="w-full" src="/assets/hub-player.png" />
+          <h3 class="mt-2 text-xl font-bold text-black">Are you a player?</h3>
+          <p class="mt-1 text-sm text-gray-700">
+            Manage your memberships, accreditation and payments in one place.
+          </p>
+          <Show
+            when={store?.data?.player}
+            fallback={
+              <A
+                href="/dashboard"
+                class="mt-2 block w-fit rounded-lg bg-violet-600 px-4 py-2 text-sm text-white"
+              >
+                Register on Hub
+              </A>
+            }
+          >
+            <A
+              href="/dashboard"
+              class="mt-2 block w-fit rounded-lg bg-violet-600 px-4 py-2 text-sm text-white"
+            >
+              My Account
+            </A>
+          </Show>
+        </div>
+      </div>
+
+      <div class="hidden rounded-lg bg-blue-200 md:block">
+        <div class="flex items-center justify-between p-6">
+          <div>
+            <h3 class="mt-2 text-4xl font-bold text-black">
+              Are you a player?
+            </h3>
+            <p class="mt-3 text-lg text-gray-700">
+              Manage your memberships, accreditation and payments in one place.
+            </p>
+            <Show
+              when={store?.data?.player}
+              fallback={
+                <A
+                  href="/dashboard"
+                  class="mt-3 block w-fit rounded-lg bg-violet-600 px-8 py-2 text-sm text-white"
+                >
+                  Register on Hub
+                </A>
+              }
+            >
+              <A
+                href="/dashboard"
+                class="mt-3 block w-fit rounded-lg bg-violet-600 px-8 py-2 text-sm text-white"
+              >
+                My Account
+              </A>
+            </Show>
+          </div>
+          <img class="w-1/3" src="/assets/hub-player.png" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PlayerSection;
---
+++ frontend/src/components/home/TournamentSection.js
@@ -0,0 +1,122 @@
+import { A } from "@solidjs/router";
+import { createQuery } from "@tanstack/solid-query";
+import { createEffect, createSignal, For, Show } from "solid-js";
+
+import { ChevronRight } from "../../icons";
+import { fetchTournaments } from "../../queries";
+
+const TournamentSection = () => {
+  const tournamentsQuery = createQuery(() => ["tournaments"], fetchTournaments);
+  const [upcoming, setUpcoming] = createSignal([]);
+  const [past, setPast] = createSignal([]);
+
+  createEffect(() => {
+    let pastTournaments = [];
+    let upcomingTournaments = [];
+    const today = new Date();
+
+    tournamentsQuery.data?.forEach(tournament => {
+      const startDate = new Date(Date.parse(tournament.event.start_date));
+      const endDate = new Date(Date.parse(tournament.event.end_date));
+
+      if (endDate < today) {
+        pastTournaments.push(tournament);
+      } else if (startDate > today) {
+        upcomingTournaments.push(tournament);
+      }
+    });
+
+    pastTournaments.sort(
+      (a, b) => new Date(b.event.end_date) - new Date(a.event.end_date)
+    );
+
+    const lastThreeMonthTournaments = pastTournaments.filter(
+      tournament =>
+        tournament.event.end_date > today.setMonth(today.getMonth() - 3)
+    );
+
+    if (lastThreeMonthTournaments.length > 0) {
+      setPast(lastThreeMonthTournaments);
+    } else {
+      setPast(pastTournaments.slice(0, 2));
+    }
+
+    upcomingTournaments.sort(
+      (a, b) => new Date(a.event.end_date) - new Date(b.event.end_date)
+    );
+
+    setUpcoming(upcomingTournaments);
+  });
+
+  return (
+    <>
+      <div class="mt-2 flex items-center justify-between">
+        <h2 class="text-lg font-bold text-gray-700">Tournaments</h2>
+        <A href="/tournaments" class="text-sm text-blue-600 md:text-base">
+          <span class="inline-flex items-center">
+            View all <ChevronRight height={16} width={16} />
+          </span>
+        </A>
+      </div>
+      <Show when={upcoming().length > 0}>
+        <h2 class="mt-2 text-sm">Upcoming Tournaments</h2>
+        <For each={upcoming()}>
+          {tournament => <TournamentCard tournament={tournament} />}
+        </For>
+      </Show>
+      <Show when={past().length > 0}>
+        <h2 class="mt-2 text-sm">Past Tournaments</h2>
+        <For each={past()}>
+          {tournament => <TournamentCard tournament={tournament} />}
+        </For>
+      </Show>
+    </>
+  );
+};
+
+const TournamentCard = props => (
+  <A
+    class="my-2 flex items-center gap-3 rounded-md bg-gray-100 p-3 md:gap-6"
+    href={"/tournament/" + props.tournament.event.ultimate_central_slug}
+  >
+    <Show when={props.tournament.logo_light}>
+      <img src={props.tournament.logo_light} class="w-12 rounded-sm md:w-24" />
+    </Show>
+
+    <div>
+      <h3 class="text-sm font-bold text-gray-700 md:text-base">
+        {props.tournament.event.title}
+      </h3>
+      <h4 class="mt-1 text-xs">{props.tournament.event.location}</h4>
+      <h4 class="mt-1 text-xs">
+        {new Date(
+          Date.parse(props.tournament.event.start_date)
+        ).toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+          timeZone: "UTC"
+        })}
+        <Show
+          when={
+            props.tournament.event.start_date !==
+            props.tournament.event.end_date
+          }
+        >
+          {" "}
+          to{" "}
+          {new Date(
+            Date.parse(props.tournament.event.end_date)
+          ).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            timeZone: "UTC"
+          })}
+        </Show>
+      </h4>
+    </div>
+  </A>
+);
+
+export default TournamentSection;
