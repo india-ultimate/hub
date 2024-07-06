f480cf814a7965ebbad006a5b7b348f18045de61
Jonathan Samuel <jonathansamuel2k@gmail.com>
2024-07-06 18:45:05 +0530

    slugs: Handle max length

--- server/migrations/0068_event_copy_uc_slug_field.py
+++ server/migrations/0068_event_copy_uc_slug_field.py
@@ -3,10 +3,12 @@ from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 from django.db.migrations.state import StateApps
 from django.db.models import F
 
+from server.utils import slugify_max
+
 
 def copy_field(apps: StateApps, schema: BaseDatabaseSchemaEditor) -> None:
     Event = apps.get_model("server", "Event")  # noqa: N806
-    Event.objects.all().update(slug=F("ultimate_central_slug"))
+    Event.objects.all().update(slug=slugify_max(str(F("ultimate_central_slug"))))
 
 
 class Migration(migrations.Migration):
