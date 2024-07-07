92fa617e9f1e20aa7404ed090362fcdb98aa4b09
SibiAkkash <sibi.akkash@gmail.com>
2024-07-07 23:02:26 +0530

    rostering: Link to roster page in tournament registration

--- frontend/src/components/TeamRegistration.js
+++ frontend/src/components/TeamRegistration.js
@@ -256,20 +256,21 @@ const TeamRegistration = () => {
                       <img src={team.image_url} class="h-8 w-8 rounded-full" />
                       <span class="font-medium">{team.name}</span>
                     </div>
-                    <button
-                      disabled // todo: Navigate to rostering page
-                      class={clsx(
-                        "inline-flex items-center justify-self-end rounded-lg px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-4",
-                        "bg-blue-500  hover:bg-blue-600 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800",
-                        "disabled:cursor-not-allowed disabled:bg-gray-400"
-                      )}
-                    >
-                      <span class="mr-2 self-center">Roster</span>
-                      <Icon
-                        path={arrowUpRight}
-                        class="mb-1 w-3 text-gray-100"
-                      />
-                    </button>
+                    <div class="justify-self-end">
+                      <A
+                        href={`/tournament/${params.slug}/team/${team.slug}/roster`}
+                        class={clsx(
+                          "inline-flex items-center rounded-lg px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-4",
+                          "bg-blue-500  hover:bg-blue-600 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        )}
+                      >
+                        <span class="mr-2 self-center">Roster</span>
+                        <Icon
+                          path={arrowUpRight}
+                          class="mb-1 w-3 text-gray-100"
+                        />
+                      </A>
+                    </div>
                   </div>
                 )}
               </For>
