43a192b6e024e9343bb13327070699ec50c6f82e
SibiAkkash <sibi.akkash@gmail.com>
2024-07-12 22:57:30 +0530

    roster: Add components for UCRegistration and Registration

---
+++ frontend/src/components/roster/UCRegistration.js
@@ -0,0 +1,48 @@
+import { Show } from "solid-js";
+
+import CaptainBadge from "./role-badges/Captain";
+import SpiritCaptainBadge from "./role-badges/SpiritCaptain";
+
+const isCaptain = registration => {
+  return (
+    registration?.roles?.includes("captain") ||
+    registration?.roles?.includes("Captain")
+  );
+};
+
+const isSpiritCaptain = registration => {
+  return (
+    registration?.roles?.includes("spirit captain") ||
+    registration?.roles?.includes("Spirit Captain")
+  );
+};
+
+const UCRegistration = props => (
+  <div class="mr-4 flex items-center space-x-4 py-3 pr-2">
+    <div class="flex items-center gap-x-4">
+      <img
+        class="h-10 w-10 rounded-full p-1 ring-2 ring-gray-300 dark:ring-gray-500"
+        src={props.registration.person.image_url}
+        alt="Bordered avatar"
+      />
+      <div class="font-medium">
+        <div>
+          {props.registration.person.first_name +
+            " " +
+            props.registration.person.last_name}
+          <Show
+            when={props.registration.person?.player?.gender}
+          >{` (${props.registration.person?.player?.gender})`}</Show>
+        </div>
+      </div>
+      <Show when={isCaptain(props.registration)}>
+        <CaptainBadge />
+      </Show>
+      <Show when={isSpiritCaptain(props.registration)}>
+        <SpiritCaptainBadge />
+      </Show>
+    </div>
+  </div>
+);
+
+export default UCRegistration;
---
+++ frontend/src/components/roster/role-badges/Manager.js
@@ -0,0 +1,6 @@
+export const ManagerBadge = () => (
+  <span class="me-2 h-fit rounded-full bg-yellow-100 px-2.5 py-0.5 text-center text-xs text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300">
+    Manager
+  </span>
+);
+export default ManagerBadge;
