7976c5042edbe8e33a89ab888eaf6c4c0fcc26c6
SibiAkkash <sibi.akkash@gmail.com>
2024-07-13 11:25:38 +0530

    roster: Use role badge components

--- frontend/src/components/roster/Registration.js
+++ frontend/src/components/roster/Registration.js
@@ -15,29 +15,27 @@ const isCoach = registration =>
 const isManager = registration => registration?.role === "MNGR";
 
 const Registration = props => (
-  <div class="mr-6 flex w-full items-center justify-between space-x-4 py-3 pr-2">
-    <div class="flex items-center gap-x-4">
-      <div class="font-medium">
-        <div>
-          {props.registration?.player?.full_name}
-          <Show
-            when={props.registration.player?.gender}
-          >{` (${props.registration.player?.gender})`}</Show>
-        </div>
+  <div class="mr-6 flex w-full items-center gap-x-4 py-3 pr-2">
+    <div class="font-medium">
+      <div>
+        {props.registration?.player?.full_name}
+        <Show
+          when={props.registration.player?.gender}
+        >{` (${props.registration.player?.gender})`}</Show>
       </div>
-      <Show when={isCaptain(props.registration)}>
-        <CaptainBadge />
-      </Show>
-      <Show when={isSpiritCaptain(props.registration)}>
-        <SpiritCaptainBadge />
-      </Show>
-      <Show when={isManager(props.registration)}>
-        <ManagerBadge />
-      </Show>
-      <Show when={isCoach(props.registration)}>
-        <CoachBadge />
-      </Show>
     </div>
+    <Show when={isCaptain(props.registration)}>
+      <CaptainBadge />
+    </Show>
+    <Show when={isSpiritCaptain(props.registration)}>
+      <SpiritCaptainBadge />
+    </Show>
+    <Show when={isManager(props.registration)}>
+      <ManagerBadge />
+    </Show>
+    <Show when={isCoach(props.registration)}>
+      <CoachBadge />
+    </Show>
   </div>
 );
 
