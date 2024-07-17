c9db325ba093f26de7ea617507b6d3164fe2fc57
SibiAkkash <sibi.akkash@gmail.com>
2024-07-17 19:32:37 +0530

    submit-scores: Change modals to render children

--- frontend/src/components/match/SubmitSpiritScore.js
+++ frontend/src/components/match/SubmitSpiritScore.js
@@ -1,14 +1,14 @@
 import clsx from "clsx";
 import { Icon } from "solid-heroicons";
+import { arrowRight } from "solid-heroicons/solid";
 
 import Modal from "../Modal";
 
-const SubmitScoreModal = props => {
+const SubmitSpiritScoreModal = props => {
   let modalRef;
   return (
     <>
       <button
-        type="button"
         class={clsx(
           "relative mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg p-0.5 font-medium",
           "text-xs text-gray-900 hover:text-white focus:outline-none focus:ring-4 dark:text-white",
@@ -17,16 +17,15 @@ const SubmitScoreModal = props => {
         onClick={() => modalRef.showModal()}
       >
         <span class="relative inline-flex items-center rounded-md bg-white px-3 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-800">
-          {props.button.text}
-          <Icon path={props.button.icon} class="ml-1.5 w-4" />
+          Submit Spirit Score
+          <Icon path={arrowRight} class="ml-1.5 w-4" />
         </span>
       </button>
-
       <Modal
         ref={modalRef}
         title={
           <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-            {props.button.text}
+            Submit Spirit score
           </h3>
         }
         close={() => {
@@ -40,4 +39,4 @@ const SubmitScoreModal = props => {
   );
 };
 
-export default SubmitScoreModal;
+export default SubmitSpiritScoreModal;
