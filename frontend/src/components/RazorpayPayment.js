f8926150ddd993202b4ce5331d7e46e6465f3634
Jonathan Samuel <jonathansamuel2k@gmail.com>
2024-08-12 23:20:53 +0530

    razorpay: Add disabled styles

--- frontend/src/components/RazorpayPayment.js
+++ frontend/src/components/RazorpayPayment.js
@@ -117,8 +117,10 @@ const RazorpayPayment = props => {
         <Spinner />
       </Show>
       <button
-        class={`my-5 block rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ${
-          props.disabled ? "cursor-not-allowed" : ""
+        class={`my-5 block rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800  ${
+          props.disabled
+            ? "cursor-not-allowed bg-gray-400 hover:bg-gray-500"
+            : ""
         }`}
         type="button"
         disabled={props.disabled}
