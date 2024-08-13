bca1c406e788c7c824e00e0d4f5bd855bed39d93
Jonathan Samuel <jonathansamuel2k@gmail.com>
2024-08-13 15:47:06 +0530

    membership: Update fee amounts

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -113,8 +113,8 @@ export const tournamentTypeChoices = [
 // NOTE: Memberships go from 1st June to 31st May
 export const membershipStartDate = [1, 6];
 export const membershipEndDate = [31, 5];
-export const annualMembershipFee = 650 * 100; // needs to be in Paise, not Rupees
-export const sponsoredAnnualMembershipFee = 350 * 100; // needs to be in Paise, not Rupees
+export const annualMembershipFee = 700 * 100; // needs to be in Paise, not Rupees
+export const sponsoredAnnualMembershipFee = 200 * 100; // needs to be in Paise, not Rupees
 export const eventMembershipFee = 375 * 100; // needs to be in Paise, not Rupees
 
 export const minAge = 12; // Minimum age to register a player
