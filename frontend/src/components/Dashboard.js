c681c08f70a963e95fc226d4f8a5205cca87ea2b
Jonathan Samuel <jonathansamuel2k@gmail.com>
2024-08-14 00:27:00 +0530

    dashboard: Update membership url for group

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -58,10 +58,10 @@ const Actions = props => {
         Import players information from a Spreadsheet
       </A>
       <A
-        href="/membership/group"
+        href={`/membership/${props.player?.id}`}
         class="block w-full cursor-pointer border-b border-gray-200 px-4 py-2 hover:bg-gray-100 hover:text-blue-700 focus:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-gray-500"
       >
-        Pay the membership fee for a group
+        Pay the membership fee for yourself or a group
       </A>
       <A
         href="/validate-rosters"
