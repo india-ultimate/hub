152f2bb396ae03b099e43d69fc568128cf6063d9
Jonathan Samuel <jonathansamuel2k@gmail.com>
2024-08-16 23:52:53 +0530

    match-stats: All event form ui

---
+++ frontend/src/components/match/stats/DropForm.js
@@ -0,0 +1,116 @@
+import { createForm, required } from "@modular-forms/solid";
+import { createMutation, createQuery } from "@tanstack/solid-query";
+import { createSignal, Show } from "solid-js";
+
+import {
+  createMatchStatsEvent,
+  fetchTournamentTeamBySlug
+} from "../../../queries";
+import Error from "../../alerts/Error";
+import Info from "../../alerts/Info";
+import Select from "../../Select";
+
+const DropForm = props => {
+  const [status, setStatus] = createSignal("");
+  const [error, setError] = createSignal("");
+
+  const rosterQuery = createQuery(
+    () => ["tournament-roster", props.tournament?.slug, props.team?.slug],
+    () =>
+      fetchTournamentTeamBySlug(
+        props.tournament?.event?.slug,
+        props.team?.slug,
+        props.tournament?.use_uc_registrations
+      ),
+    {
+      get enabled() {
+        return props.tournament?.use_uc_registrations !== undefined;
+      }
+    }
+  );
+
+  const initialValues = {
+    drop_by_id: null
+  };
+
+  const [_tournamentForm, { Form, Field }] = createForm({
+    initialValues,
+    validateOn: "touched",
+    revalidateOn: "touched"
+  });
+
+  const createMatchStatsEventMutation = createMutation({
+    mutationFn: createMatchStatsEvent,
+    onSuccess: () => {
+      setStatus("Saved! You can now close this.");
+    },
+    onError: e => {
+      console.log(e);
+      setError(e);
+    }
+  });
+
+  const handleSubmit = async values => {
+    setStatus("");
+    setError("");
+
+    createMatchStatsEventMutation.mutate({
+      match_id: props.match?.id,
+      body: {
+        type: "DR",
+        team_id: props.team?.id,
+        ...values
+      }
+    });
+  };
+
+  return (
+    <Form
+      class="mt-12 space-y-12 md:space-y-14 lg:space-y-16"
+      onSubmit={values => handleSubmit(values)}
+    >
+      <div class="space-y-8">
+        <Field
+          name="drop_by_id"
+          validate={required("Please add the player who dropped.")}
+        >
+          {(field, fieldProps) => (
+            <Select
+              {...fieldProps}
+              value={field.value}
+              error={field.error}
+              options={rosterQuery.data?.map(r => {
+                return {
+                  value: props.tournament?.use_uc_registrations
+                    ? r.person.id.toString()
+                    : r.player.id.toString(),
+                  label: props.tournament?.use_uc_registrations
+                    ? r.person.first_name + " " + r.person.last_name
+                    : r.player.full_name
+                };
+              })}
+              required
+              type="text"
+              label="Drop By"
+              placeholder="Choose a Player"
+            />
+          )}
+        </Field>
+        <button
+          type="submit"
+          class="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
+        >
+          Submit
+        </button>
+        <Show when={error()}>
+          <Error text={`Oops ! ${error()}`} />
+        </Show>
+        <Show when={status()}>
+          <Info text={status()} />
+        </Show>
+      </div>
+    </Form>
+  );
+};
+
+export default DropForm;
---
+++ frontend/src/components/match/stats/ScoreForm.js
@@ -0,0 +1,143 @@
+import { createForm, required } from "@modular-forms/solid";
+import { createMutation, createQuery } from "@tanstack/solid-query";
+import { createSignal, Show } from "solid-js";
+
+import {
+  createMatchStatsEvent,
+  fetchTournamentTeamBySlug
+} from "../../../queries";
+import Error from "../../alerts/Error";
+import Info from "../../alerts/Info";
+import Select from "../../Select";
+
+const ScoreForm = props => {
+  const [status, setStatus] = createSignal("");
+  const [error, setError] = createSignal("");
+
+  const rosterQuery = createQuery(
+    () => ["tournament-roster", props.tournament?.slug, props.team?.slug],
+    () =>
+      fetchTournamentTeamBySlug(
+        props.tournament?.event?.slug,
+        props.team?.slug,
+        props.tournament?.use_uc_registrations
+      ),
+    {
+      get enabled() {
+        return props.tournament?.use_uc_registrations !== undefined;
+      }
+    }
+  );
+
+  const initialValues = {
+    scored_by_id: null,
+    assisted_by_id: null
+  };
+
+  const [_tournamentForm, { Form, Field }] = createForm({
+    initialValues,
+    validateOn: "touched",
+    revalidateOn: "touched"
+  });
+
+  const createMatchStatsEventMutation = createMutation({
+    mutationFn: createMatchStatsEvent,
+    onSuccess: () => {
+      setStatus("Saved! You can now close this.");
+    },
+    onError: e => {
+      console.log(e);
+      setError(e);
+    }
+  });
+
+  const handleSubmit = async values => {
+    setStatus("");
+    setError("");
+
+    createMatchStatsEventMutation.mutate({
+      match_id: props.match?.id,
+      body: {
+        type: "SC",
+        team_id: props.team?.id,
+        ...values
+      }
+    });
+  };
+
+  return (
+    <Form
+      class="mt-12 space-y-12 md:space-y-14 lg:space-y-16"
+      onSubmit={values => handleSubmit(values)}
+    >
+      <div class="space-y-8">
+        <Field
+          name="scored_by_id"
+          validate={required("Please add the player who scored.")}
+        >
+          {(field, fieldProps) => (
+            <Select
+              {...fieldProps}
+              value={field.value}
+              error={field.error}
+              options={rosterQuery.data?.map(r => {
+                return {
+                  value: props.tournament?.use_uc_registrations
+                    ? r.person.id.toString()
+                    : r.player.id.toString(),
+                  label: props.tournament?.use_uc_registrations
+                    ? r.person.first_name + " " + r.person.last_name
+                    : r.player.full_name
+                };
+              })}
+              required
+              type="text"
+              label="Scored By"
+              placeholder="Choose a Player"
+            />
+          )}
+        </Field>
+        <Field
+          name="assisted_by_id"
+          validate={required("Please add the player who assisted.")}
+        >
+          {(field, fieldProps) => (
+            <Select
+              {...fieldProps}
+              value={field.value}
+              error={field.error}
+              options={rosterQuery.data?.map(r => {
+                return {
+                  value: props.tournament?.use_uc_registrations
+                    ? r.person.id.toString()
+                    : r.player.id.toString(),
+                  label: props.tournament?.use_uc_registrations
+                    ? r.person.first_name + " " + r.person.last_name
+                    : r.player.full_name
+                };
+              })}
+              type="text"
+              required
+              label="Assisted By"
+              placeholder="Choose a Player"
+            />
+          )}
+        </Field>
+        <button
+          type="submit"
+          class="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
+        >
+          Submit
+        </button>
+        <Show when={error()}>
+          <Error text={`Oops ! ${error()}`} />
+        </Show>
+        <Show when={status()}>
+          <Info text={status()} />
+        </Show>
+      </div>
+    </Form>
+  );
+};
+
+export default ScoreForm;
