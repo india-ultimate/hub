e234e71c717349a8aee4d33c88a6219c3a66419d
SibiAkkash <sibi.akkash@gmail.com>
2024-08-20 07:40:26 +0530

    season: Add series to season schema

--- server/season/schema.py
+++ server/season/schema.py
@@ -1,9 +1,19 @@
+from django.db.models import QuerySet
 from ninja import ModelSchema
+
+from server.series.models import Series
+from server.series.schema import SeriesMinSchema
 
 from .models import Season
 
 
 class SeasonSchema(ModelSchema):
+    series: list[SeriesMinSchema]
+
+    @staticmethod
+    def resolve_series(season: Season) -> QuerySet[Series]:
+        return season.series.all()
+
     class Config:
         model = Season
         model_fields = "__all__"
