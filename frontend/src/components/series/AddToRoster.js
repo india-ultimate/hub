d2d86d46e305153ce6740153228b1280d57143a8
Jonathan Samuel <jonathansamuel2k@gmail.com>
2024-08-24 00:23:39 +0530

    invite-player: Add loading state in buttons

--- frontend/src/components/series/AddToRoster.js
+++ frontend/src/components/series/AddToRoster.js
@@ -208,9 +208,15 @@ const AddPlayerRegistrationForm = componentProps => {
                   }
                 })
               }
-              class="mb-2 me-2 rounded-lg border border-blue-700 px-5 py-1.5 text-center text-sm font-medium text-blue-700 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+              disabled={invitePlayerMutation.isLoading}
+              class="mb-2 me-2 rounded-lg border border-blue-700 px-5 py-1.5 text-center text-sm font-medium text-blue-700 hover:bg-blue-800 hover:text-white disabled:text-blue-400"
             >
-              Invite
+              <Show
+                when={!invitePlayerMutation.isLoading}
+                fallback={"Loading..."}
+              >
+                Invite
+              </Show>
             </button>
           </Match>
         </Switch>
