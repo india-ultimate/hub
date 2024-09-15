6d52bde55dff7e14044ac31b79dda640cd866d37
supergi0 <sagarpingale0110@gmail.com>
2024-09-15 20:20:38 +0530

    series-reg: Different email redirect link in case of DEBUG or not DEBUG

--- hub/settings.py
+++ hub/settings.py
@@ -186,7 +186,11 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_SECRET_KEY = os.environ.get("EMAIL_SECRET_KEY")
-EMAIL_INVITATION_BASE_URL = "http://localhost:3000"
+
+if DEBUG:
+    EMAIL_INVITATION_BASE_URL = f"http://localhost:{os.environ.get('WEBPACK_SERVER_PORT', 3000)}"
+else:
+    EMAIL_INVITATION_BASE_URL = "https://hub.indiaultimate.org"
 
 # OTP settings
 OTP_EMAIL_HASH_KEY = os.environ.get("OTP_EMAIL_HASH_KEY", "")
