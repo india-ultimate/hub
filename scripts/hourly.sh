0d279bafb34934326777a1b2f2d8081529dcda26
Jonathan Samuel <jonathansamuel2k@gmail.com>
2024-10-02 19:53:15 +0530

    cron: Disable uc events and regs import

--- scripts/hourly.sh
+++ scripts/hourly.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python manage.py import_uc_events -n 20
-python manage.py import_uc_registrations --since 2023-06-01
-python manage.py reconcile_phonepe_transactions
+# python manage.py import_uc_events -n 20
+# python manage.py import_uc_registrations --since 2023-06-01
+# python manage.py reconcile_phonepe_transactions
