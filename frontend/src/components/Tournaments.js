70a75dbf656cf677a87bab4b4bd9a6db1b9ea0db
Jonathan Samuel <jonathansamuel2k@gmail.com>
2024-12-29 10:51:03 +0530

    tournaments: Add filters based on type

--- frontend/src/components/Tournaments.js
+++ frontend/src/components/Tournaments.js
@@ -1,12 +1,17 @@
 import { A } from "@solidjs/router";
 import { createQuery } from "@tanstack/solid-query";
 import clsx from "clsx";
-import { For, Match, Show, Switch } from "solid-js";
+import { createSignal, For, Match, Show, Switch } from "solid-js";
 
 import { fetchTournaments } from "../queries";
 
 const Tournaments = () => {
   const tournamentsQuery = createQuery(() => ["tournaments"], fetchTournaments);
+
+  const [showMixed, setShowMixed] = createSignal(true);
+  const [showOpens, setShowOpens] = createSignal(true);
+  const [showWomens, setShowWomens] = createSignal(true);
+
   return (
     <div>
       <h1 class="mb-5 text-center">
@@ -14,12 +19,77 @@ const Tournaments = () => {
           Tournaments
         </span>
       </h1>
+
+      <ul class="flex w-full items-center rounded-lg border border-blue-500 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white">
+        <li class="w-full border-b-0 border-r border-blue-500 dark:border-gray-600">
+          <div class="flex items-center ps-3">
+            <input
+              id="mixed-checkbox"
+              type="checkbox"
+              value="mixed"
+              checked={showMixed()}
+              onChange={e => setShowMixed(e.target.checked)}
+              class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
+            />
+            <label
+              for="mixed-checkbox"
+              class="ms-2 w-full py-3 text-sm font-medium text-blue-600 dark:text-gray-300"
+            >
+              Mixed
+            </label>
+          </div>
+        </li>
+        <li class="w-full border-b-0 border-r border-blue-500 dark:border-gray-600">
+          <div class="flex items-center ps-3">
+            <input
+              id="opens-checkbox"
+              type="checkbox"
+              value="mens"
+              checked={showOpens()}
+              onChange={e => setShowOpens(e.target.checked)}
+              class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
+            />
+            <label
+              for="opens-checkbox"
+              class="ms-2 w-full py-3 text-sm font-medium text-blue-600 dark:text-gray-300"
+            >
+              Opens
+            </label>
+          </div>
+        </li>
+        <li class="w-full border-b-0 border-blue-500 dark:border-gray-600">
+          <div class="flex items-center ps-3">
+            <input
+              id="womens-checkbox"
+              type="checkbox"
+              value="womens"
+              checked={showWomens()}
+              onChange={e => setShowWomens(e.target.checked)}
+              class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-700"
+            />
+            <label
+              for="womens-checkbox"
+              class="ms-2 w-full py-3 text-sm font-medium text-blue-600 dark:text-gray-300"
+            >
+              Womens
+            </label>
+          </div>
+        </li>
+      </ul>
+
       <div
         class={
           "mt-5 grid grid-cols-1 justify-items-center gap-5 md:grid-cols-2 lg:grid-cols-3 "
         }
       >
-        <For each={tournamentsQuery.data}>
+        <For
+          each={tournamentsQuery.data?.filter(t => {
+            if (t.event?.type === "OPN" && showOpens()) return true;
+            if (t.event?.type === "MXD" && showMixed()) return true;
+            if (t.event?.type === "WMN" && showWomens()) return true;
+            return false;
+          })}
+        >
           {tournament => (
             <Show when={tournament.status !== "DFT"}>
               <A
