e91e81244f77b692bfce55798bd7075e04f1207d
Jonathan Samuel <jonathansamuel2k@gmail.com>
2025-07-09 12:20:07 +0530

    election: Add for guardians to vote on behalf of their wards

--- frontend/src/components/alerts/Error.js
+++ frontend/src/components/alerts/Error.js
@@ -13,7 +13,7 @@ const Error = props => (
       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
     </svg>
     <span class="sr-only">Info</span>
-    <div>{props.text}</div>
+    <div class="w-5/6 whitespace-pre-wrap break-words">{props.text}</div>
   </div>
 );
 
