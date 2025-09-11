c9179e28818f4cf477cc47856caa1c2ba0fa8f2e
Jonathan Samuel <jonathansamuel2k@gmail.com>
2025-09-11 09:14:36 -0700

    servicerequest: Add players field and update apis

--- server/servicerequests/schema.py
+++ server/servicerequests/schema.py
@@ -1,14 +1,21 @@
 from ninja import ModelSchema, Schema
 
+from server.schema import PlayerMinSchema
+
 from .models import ServiceRequest
 
 
 class ServiceRequestSchema(ModelSchema):
     user_full_name: str
+    service_players: list[PlayerMinSchema]
 
     @staticmethod
     def resolve_user_full_name(service_request: ServiceRequest) -> str:
         return service_request.user.get_full_name()
+
+    @staticmethod
+    def resolve_service_players(service_request: ServiceRequest) -> list[PlayerMinSchema]:
+        return list(service_request.service_players.all())
 
     class Config:
         model = ServiceRequest
@@ -18,3 +25,4 @@ class ServiceRequestSchema(ModelSchema):
 class ServiceRequestCreateSchema(Schema):
     type: str
     message: str
+    service_player_ids: list[int] | None = None
