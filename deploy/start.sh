5c1c9d342ec9187b328ccfdd57798a8b9d4be98d
Jonathan Samuel <jonathansamuel2k@gmail.com>
2025-09-30 19:01:11 -0700

    worker: Add as systemd service

--- deploy/start.sh
+++ deploy/start.sh
@@ -10,11 +10,26 @@ sudo cron
 HERE=$(dirname "$0")
 "$HERE/make_cron_env.py"
 
+# Setup env vars for worker service
+echo "Setting up worker environment..."
+"$HERE/make_worker_env.py"
+
 # Migrate DB
 python manage.py migrate
 
+# Install and setup worker systemd service
+echo "Installing Hub worker systemd service..."
+sudo cp "$HERE/hub-worker.service" /etc/systemd/system/
+sudo mkdir -p /etc/hub
+
 # Ensure no security check errors
 python manage.py check --deploy
+
+# Start the worker service
+echo "Starting Hub worker service..."
+sudo systemctl daemon-reload
+sudo systemctl enable hub-worker
+sudo systemctl start hub-worker
 
 # Start the server using gunicorn
 export PATH="$HOME/.local/bin:$PATH"
