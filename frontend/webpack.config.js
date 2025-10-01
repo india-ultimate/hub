98b1afd50a82b49f849a8fa031304821dba3112f
Jonathan Samuel <jonathansamuel2k@gmail.com>
2025-09-30 20:08:32 -0700

    proxy: Fix ckeditor url in nginx and webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -88,7 +88,8 @@ module.exports = {
       "/api": "http://127.0.0.1:8000",
       "/admin": "http://127.0.0.1:8000",
       "/static": "http://127.0.0.1:8000",
-      "/uploads": "http://127.0.0.1:8000"
+      "/uploads": "http://127.0.0.1:8000",
+      "/ckeditor": "http://127.0.0.1:8000"
     }
   }
 };
