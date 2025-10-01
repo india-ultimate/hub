1e2e7bf031696494c910ce4573dadb9372dde922
Jonathan Samuel <jonathansamuel2k@gmail.com>
2025-09-30 19:19:36 -0700

    deploy: Switch to tmux for worker

--- deploy/start.sh
+++ deploy/start.sh
@@ -10,26 +10,15 @@ sudo cron
 HERE=$(dirname "$0")
 "$HERE/make_cron_env.py"
 
-# Install and setup worker systemd service
-echo "Installing Hub worker systemd service..."
-sudo cp "$HERE/hub-worker.service" /etc/systemd/system/
-sudo mkdir -p /etc/hub
-
-# Setup env vars for worker service
-echo "Setting up worker environment..."
-"$HERE/make_worker_env.py"
-
 # Migrate DB
 python manage.py migrate
 
 # Ensure no security check errors
 python manage.py check --deploy
 
-# Start the worker service
-echo "Starting Hub worker service..."
-sudo systemctl daemon-reload
-sudo systemctl enable hub-worker
-sudo systemctl start hub-worker
+# Start worker processes using tmux
+echo "Starting Hub worker processes..."
+tmux new-session -d -s hub-worker "python manage.py run_task_worker --sleep-seconds 30"
 
 # Start the server using gunicorn
 export PATH="$HOME/.local/bin:$PATH"
